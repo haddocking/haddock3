fb3113816a4fd6ad2e64f60bee320d486e3068d6
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2021-07-13 12:09:00 +0200

    Update parallel.py

--- haddock/parallel.py
+++ haddock/parallel.py
@@ -6,31 +6,36 @@ from multiprocessing import Process, cpu_count
 logger = logging.getLogger(__name__)
 
 
-class Sailor(Process):
-    """What is a captain without sailors?"""
+class Worker(Process):
+
     def __init__(self, tasks):
-        super(Sailor, self).__init__()
+        super(Worker, self).__init__()
         self.tasks = tasks
-        logger.info(f"Sailor ready with {len(self.tasks)} tasks")
+        logger.info(f"Worker ready with {len(self.tasks)} tasks")
 
     def run(self):
         for task in self.tasks:
             task.run()
-        logger.info(f"Sailor {self.name} says: Aye, Captain!")
+        logger.info(f"{self.name} executed")
 
 
-class CaptainHaddock():
-    """Aye, Captain"""
+class Scheduler():
+
     def __init__(self, tasks, num_cores=0):
         try:
             self.num_processes = int(num_cores)
+
             if self.num_processes < 1:
                 raise ValueError()
+
             if self.num_processes > cpu_count():
-                logger.warning(f"Number of cores ({self.num_processes}) larger than available.")
+                logger.warning(f"Number of cores ({self.num_processes}) larger"
+                               " than available.")
                 raise ValueError()
+
         except (ValueError, TypeError):
-            logger.warning("Number of cores has not been specified or is incorrect. Using available cores.")
+            logger.warning("Number of cores has not been specified or is"
+                           " incorrect. Using available cores.")
             self.num_processes = cpu_count()
 
         self.tasks = tasks
@@ -38,34 +43,38 @@ class CaptainHaddock():
         # Do not waste resources
         if self.num_tasks < self.num_processes:
             self.num_processes = self.num_tasks
-        logger.info(f"Captain commands {self.num_processes} sailors (cpu cores)")
+        logger.info(f"Running with {self.num_processes} cpu cores")
 
-        self.crew = []
-        crew_tasks = [tasks[i::self.num_processes] for i in range(self.num_processes)]
+        self.task_list = []
+        job_list = []
+        for i in range(self.num_processes):
+            job_list.append(tasks[i::self.num_processes])
 
         for i in range(self.num_processes):
-            sailor = Sailor(crew_tasks[i])
-            self.crew.append(sailor)
+            task = Worker(job_list[i])
+            self.task_list.append(task)
 
         logger.info(f"{self.num_tasks} tasks ready")
 
-    def drink(self):
-        """Drink up me "earties, yo ho"""
-        logger.info("Oh Captain, my Captain!")
-        try:
-            for sailor in self.crew:
-                sailor.start()
+    def execute(self):
 
-            for sailor in self.crew:
-                sailor.join()
+        try:
+            for task in self.task_list:
+                task.start()
+
+            for task in self.task_list:
+                task.join()
 
             logger.info(f"{self.num_tasks} tasks finished")
-        except KeyboardInterrupt:
-            self.mutiny()
 
-    def mutiny(self):
-        """There is a mutiny in the ship, anyone interrupted?"""
-        logger.warning("Mutiny in the ship")
-        for sailor in self.crew:
-            sailor.terminate()
-        logger.warning("The crew has stopped working")
+        except KeyboardInterrupt:
+            # Q: why have a keyboard interrupt here?
+            self.terminate()
+
+    def terminate(self):
+
+        logger.warning("Something went wrong")
+        for task in self.task_list:
+            task.terminate()
+
+        logger.warning("The workers have stopped")

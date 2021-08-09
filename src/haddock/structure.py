aa7140f4ac6687caa14e1758f9b9a2b45fa3a30d
joaomcteixeira <joaomcteixeira@gmail.com>
2021-08-09 17:32:16 +0200

    moved method to function

--- src/haddock/structure.py
+++ src/haddock/structure.py
@@ -4,5 +4,13 @@
 class Molecule:
     """Input molecule, usually a PDB file."""
     def __init__(self, file_name, segid=None):
+        # the rest of the code is too dependent on the Path API
+        assert isinstance(file_name, Path), "`file_name` must be pathlib.Path"
+
         self.file_name = file_name
         self.segid = segid
+
+
+def make_molecules(self, paths):
+    """Get input molecules from the data stream."""
+    return list(map(Molecule, paths))

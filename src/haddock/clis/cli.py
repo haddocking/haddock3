b501e1e0e1fa20a7d89e26f7b6241de4db5e7645
joaomcteixeira <joaomcteixeira@gmail.com>
2021-09-03 17:33:52 +0200

    well structured cli

--- src/haddock/clis/cli.py
+++ src/haddock/clis/cli.py
@@ -1,80 +1,125 @@
 #!/usr/bin/env python3
-
 import argparse
 import logging
 import sys
+from functools import partial
+
 from haddock.version import CURRENT_VERSION
-from haddock.workflow import WorkflowManager
-from haddock.gear.greetings import get_adieu, get_initial_greeting
-from haddock.gear.prepare_run import setup_run
-from haddock.error import HaddockError, ConfigurationError
+from haddock.libs.libutil import non_negative_int
 
 
-def main(args=None):
+# Command line interface parser
+ap = argparse.ArgumentParser()
 
-    def positive_int(n):
-        n = int(n)
-        if n < 0:
-            raise argparse.ArgumentTypeError("Minimum value is 0")
-        return n
+ap.add_argument(
+    "recipe",
+    type=argparse.FileType("r"),
+    help="The input recipe file path",
+    )
 
-    # Command line interface parser
-    parser = argparse.ArgumentParser()
-    # Add logging to CLI parser
-    levels = ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
-    parser.add_argument("--log-level", default="INFO", choices=levels)
-    # Restart option
-    parser.add_argument("--restart", type=positive_int, default=0,
-                        help="Restart the recipe from this course")
-    # The recipe to be used
-    parser.add_argument("recipe", type=argparse.FileType("r"),
-                        help="The input recipe file name")
+_arg_pos_err = argparse.ArgumentTypeError("Minimum value is 0")
+_arg_pos_int = partial(non_negative_int, exception=_arg_pos_err)
+ap.add_argument(
+    "--restart",
+    type=_arg_pos_int,
+    default=0,
+    help="Restart the recipe from this course",
+    )
 
-    parser.add_argument(
-        "--setup",
-        help="Only setup the run, do not execute",
-        action="store_true",
-        )
+ap.add_argument(
+    "--setup",
+    help="Only setup the run, do not execute",
+    action="store_true",
+    dest='setup_only',
+    )
 
-    # Version
-    parser.add_argument("-V", "-v", "--version", help="show version",
-                        action="version",
-                        version="%s %s" % (parser.prog, CURRENT_VERSION))
+_log_levels = ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+ap.add_argument(
+    "--log-level",
+    default="INFO",
+    choices=_log_levels,
+    )
 
-    # Special case only using print instead of logging
-    options = parser.parse_args()
-    if not hasattr(options, "version"):
-        print(get_initial_greeting())
+ap.add_argument(
+    "-v",
+    "--version",
+    help="show version",
+    action="version",
+    version=f'{ap.prog} - {CURRENT_VERSION}',
+    )
+
+
+def load_args(ap):
+    """Load argument parser args."""
+    return ap.parse_args()
+
+
+def cli(ap, main):
+    """Command-line interface entry point."""
+    cmd = load_args(ap)
+    main(**vars(cmd))
+
+
+def maincli():
+    """Main client execution."""
+    cli(ap, main)
+
+
+def main(
+        recipe,
+        restart=0,
+        setup_only=False,
+        log_level="INFO",
+        ):
+    """
+    Execute HADDOCK3 client logic.
+
+    Parameters
+    ----------
+    recipe : str or pathlib.Path
+        The path to the recipe (config file).
+
+    restart : int
+    """
+
+    # anti-pattern to speed up CLI initiation
+    from haddock.workflow import WorkflowManager
+    from haddock.gear.greetings import get_adieu, get_initial_greeting
+    from haddock.gear.prepare_run import setup_run
+    from haddock.error import HaddockError, ConfigurationError
 
     # Configuring logging
     logging.basicConfig(level=options.log_level,
                         format=("[%(asctime)s] %(name)s:L%(lineno)d"
                                 " %(levelname)s - %(message)s"))
 
-    try:
-        params, other_params = setup_run(options.recipe.name)
+    # Special case only using print instead of logging
+    logging.info(get_initial_greeting())
 
-    except ConfigurationError as se:
-        logging.error(se)
+    try:
+        params, other_params = setup_run(recipe)
+
+    except ConfigurationError as err:
+        logging.error(err)
         sys.exit()
 
-    if not options.setup:
+    if not setup_only:
         try:
             workflow = WorkflowManager(
                 workflow_params=params,
-                start=options.restart,
+                start=restart,
                 **other_params,
                 )
 
             # Main loop of execution
             workflow.run()
 
-        except HaddockError as he:
-            logging.error(he)
+        except HaddockError as err:
+            logging.error(err)
 
     # Finish
     logging.info(get_adieu())
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(maincli())

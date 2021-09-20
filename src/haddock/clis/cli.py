763b6f1d830b4ad091d8a7e5b0d4f06916503d56
joaomcteixeira <joaomcteixeira@gmail.com>
2021-09-20 13:35:53 +0200

    main structure working - pre cleaning

--- src/haddock/clis/cli.py
+++ src/haddock/clis/cli.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import argparse
 import sys
+import shutil
 from argparse import ArgumentTypeError
 from functools import partial
+from pathlib import Path
 
 from haddock import current_version, has_terminal, log
 from haddock.gear.restart_run import add_restart_arg
-from haddock.libs.liblog import add_loglevel_arg, set_log_level_for_clis
+from haddock.libs.liblog import add_loglevel_arg, set_log_level_for_clis, info_name, debug_name
 from haddock.libs.libutil import file_exists
 
 
@@ -82,23 +84,53 @@ def main(
     log_level : str
         The logging level: INFO, DEBUG, ERROR, WARNING, CRITICAL.
     """
-    # anti-pattern to speed up CLI initiation
-    from haddock.libs.libworkflow import WorkflowManager
-    from haddock.gear.greetings import get_adieu, get_initial_greeting
+    # small antipatterns to improve CLI startup speed
     from haddock.gear.prepare_run import setup_run
-    from haddock.core.exceptions import HaddockError, ConfigurationError
+    from haddock.core.exceptions import ConfigurationError
 
-    set_log_level_for_clis(log, log_level, add_streamhandler=has_terminal)
-
-    # Special case only using print instead of logging
-    log.info(get_initial_greeting())
+    log_streams, log_suggested_names = \
+        set_log_level_for_clis(log, log_level, stream_to_stdout=has_terminal)
 
     try:
         params, other_params = setup_run(recipe, restart_from=restart)
 
     except ConfigurationError as err:
         log.error(err)
+        save_streams_to_files(log_streams, log_suggested_names)
         sys.exit()
+
+    try:
+        _run(recipe, restart, setup_only, params, other_params)
+    except BaseException as err:
+        log.exception(err)
+
+    _add_dir_run = lambda x: Path(other_params['run_dir'], Path(x).name)
+    _log_names = list(map(_add_dir_run, log_suggested_names))
+    save_streams_to_files(log_streams, _log_names)
+
+
+
+def save_stream_to_file(stream, file_name):
+    stream.seek(0)
+    with open(file_name, 'w') as fout:
+        shutil.copyfileobj(stream, fout)
+
+
+def save_streams_to_files(streams, file_names):
+    for stream, file_name in zip(streams, file_names):
+        save_stream_to_file(stream, file_name)
+
+
+def _run(recipe, restart, setup_only, params, other_params):
+
+    # anti-pattern to speed up CLI initiation
+    from haddock.libs.libworkflow import WorkflowManager
+    from haddock.gear.greetings import get_adieu, get_initial_greeting
+    from haddock.core.exceptions import HaddockError, ConfigurationError
+
+    # Special case only using print instead of logging
+    log.info(get_initial_greeting())
+
 
     if not setup_only:
         try:
@@ -116,6 +148,7 @@ def main(
 
     # Finish
     log.info(get_adieu())
+    return
 
 
 if __name__ == "__main__":
--- src/haddock/libs/liblog.py
+++ src/haddock/libs/liblog.py
@@ -1,5 +1,12 @@
 import logging
+import io
+import os
+import sys
+from pathlib import Path
 
+
+info_name = 'haddock3.log'
+debug_name = 'haddock3.debug'
 
 info_formatter = '[%(asctime)s]%(message)s'
 debug_formatter = \
@@ -29,52 +36,56 @@ def add_streamhandler(
         formatter=info_formatter,
         ):
     """Add streamhandler to the log object."""
-    ch = logging.StreamHandler()
+    ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(log_levels[log_level.upper()])
     ch.setFormatter(logging.Formatter(formatter))
     log.addHandler(ch)
     return
 
 
-def set_log_level_for_clis(log, log_level, add_streamhandler=True):
+def set_log_level_for_clis(log, log_level, stream_to_stdout=True):
     """."""
     log.setLevel(log_levels[log_level])
 
     if log_level.upper() in ("INFO", "WARNING", "ERROR", "CRITICAL"):
-        init_info_file(log)
+        h, n = init_info_file(log)
+        return [h.stream], [n]
 
     elif log_level.upper() in ("DEBUG"):
         log.handlers.clear()
-        if add_streamhandler:
+        if stream_to_stdout:
             add_streamhandler(log, log_level='DEBUG', formatter=debug_formatter)
-        init_log_files(log)
-        init_debug_file(log)
+        h1, n1 = init_info_file(log)
+        h2, n2 = init_debug_file(log)
+        return (h1.stream, h2.stream), (n1, n2)
 
-    return
+    else:
+        raise AssertionError("Execution shouldn't reach this point.")
 
 
 def init_debug_file(
         log,
-        file_name='haddock3.debug',
+        file_name=debug_name,
         formatter=debug_formatter,
         ):
     """."""
-    handler = logging.FileHandler(file_name, mode='w')
+    #handler = logging.FileHandler(file_name, mode='w')
+    handler = logging.StreamHandler(io.StringIO(newline=os.linesep))
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(logging.Formatter(formatter))
     log.addHandler(handler)
-    return
+    return handler, file_name
 
 
 def init_info_file(
         log,
-        file_name='haddock3.log',
+        file_name=info_name,
         formatter=info_formatter,
         ):
     """
     """
-    handler = logging.FileHandler(file_name, mode='w')
+    handler = logging.StreamHandler(io.StringIO(newline=os.linesep))
     handler.setLevel(logging.INFO)
     handler.setFormatter(logging.Formatter(formatter))
     log.addHandler(handler)
-    return
+    return handler, file_name

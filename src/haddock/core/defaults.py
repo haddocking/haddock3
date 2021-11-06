42d5b59c1ae7d10d97caef3e166183674c15877b
joaomcteixeira <joaomcteixeira@gmail.com>
2021-11-06 20:27:24 +0100

    avoids exiting when CNS not found if in TOX

--- src/haddock/core/defaults.py
+++ src/haddock/core/defaults.py
@@ -17,7 +17,8 @@ if not cns_exec.exists():
         'CNS executable `bin/cns` not found. '
         'Did you installed HADDOCK3 properly?'
         )
-    sys.exit()
+    if 'TOXTESTING' not in os.environ:
+        sys.exit()
 
 # Module input and generated data will be stored in folder starting by
 #  this prefix

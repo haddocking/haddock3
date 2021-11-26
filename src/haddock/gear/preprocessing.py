972a5d860084b1d25e0a1b30423b84742e110898
joaomcteixeira <joaomcteixeira@gmail.com>
2021-11-26 10:03:09 +0100

    solves the chainID/segID

--- src/haddock/gear/preprocessing.py
+++ src/haddock/gear/preprocessing.py
@@ -4,6 +4,7 @@ Processes input PDB files to ensure compatibility with HADDOCK3.
 
 """
 import os
+import string
 from pathlib import Path
 from functools import partial
 
@@ -15,7 +16,10 @@ from pdbtools import (
     pdb_tidy,
     )
 
-from haddock.libs.libfunc import chainf, chainfs, reduce_helper, vartial
+from haddock.libs.libfunc import chainf, chainfs, reduce_helper
+
+
+upper_case = list(string.ascii_uppercase)[::-1]
 
 
 def process_pdb_files(*files, osuffix='_processed'):
@@ -40,12 +44,14 @@ def process_pdb_files(*files, osuffix='_processed'):
 
     procfiles = process_structures(files_content)
 
+    # prepares output paths
     f_paths = map(Path, files)
     o_paths = [
         Path(f.parent, f.stem + osuffix, f.suffix)
         for f in map(Path, files)
         ]
 
+    # saves processed structures to files
     for out_p, lines in zip(o_paths, procfiles):
         out_p.write_text(''.join(lines))
 
@@ -75,7 +81,7 @@ def process_pdb(structures):
     # these are the processing or checking functions that should (if needed)
     # modify the input PDB and return the corrected lines
     # (in the like of pdbtools)
-    individual_processing_steps = [
+    line_by_line_processing_steps = [
         pdb_keepcoord.run,
         pdb_selaltloc.run,
         partial(pdb_rplresname.run, name_from='MSE', name_to='MET'),
@@ -83,7 +89,12 @@ def process_pdb(structures):
         #
         pdb_tidy.run,
         #
-        partial(map, vartial(str.rstrip, os.linesep)),
+        #partial(map, vartial(str.rstrip, os.linesep)),
+        partial(map, lambda x: x.rstrip(os.linesep)),
+        ]
+
+    whole_pdb_processing_steps = [
+        solve_no_chainID_no_segID,
         ]
 
     # these are the checks that are performed considering all the PDBs
@@ -93,7 +104,7 @@ def process_pdb(structures):
 
     # perform the individual processing steps
     processed_individually = [
-        list(chainf(structure, *individual_processing_steps))
+        list(chainf(structure, *line_by_line_processing_steps))
         for structure in structures
         ]
 
@@ -102,3 +113,41 @@ def process_pdb(structures):
         check_func(processed_individually)
 
     return processed_individually
+
+
+def solve_no_chainID_no_segID(lines):
+    """
+    Solve inconsistencies with chainID and segID.
+
+    If segID is non-existant, copy chainID over segID, and vice-verse.
+    If none are present, adds an upper case char starting from A. This
+    char is not repeated until the alphabet exhausts.
+    If chainIDs and segIDs differ, copy chainIDs over segIDs.
+
+    Returns
+    -------
+    list
+        With new lines. Or the input ones if no modification was made.
+    """
+    chainids = read_chainids(lines)
+    segids = read_segids(lines)
+
+    if not chainids and segids:
+        new_lines = pdb_segxchain.run(lines)
+
+    elif chainids and not segids:
+        new_lines = pdb_chainxseg.run(lines)
+
+    elif not chainids and not segids:
+        _chains = pdb_chain.run(lines, upper_case.pop())
+        new_lines = pdb_chainxseg.run(_chains)
+
+    # gives priority to chains
+    elif chainds != segids:
+        new_lines = pdb_chainxseg.run(lines)
+
+    else:
+        # nothing to do
+        return lines
+
+    return list(new_lines)
--- src/haddock/libs/libfunc.py
+++ src/haddock/libs/libfunc.py
@@ -6,36 +6,6 @@ From: https://github.com/joaomcteixeira/libfuncpy
 from functools import reduce
 
 
-def vartial(func, *args, **keywords):
-    """
-    Prepare a function with args and kwargs except for the first arg.
-    Functions like `functools.partial` except that the resulting
-    preprepared function expects the first positional argument.
-    Example
-    -------
-    >>> pow2 = vartial(math.pow, 2)
-    >>> pow2(3)
-    9
-    >>> pow2(4)
-    16
-    This is different from:
-    >>> pow_base_3 = partial(math.pow, 3)
-    >>> pow_base_3(2)
-    9
-    >>> pow_base_3(4)
-    81
-    """
-    def newfunc(value, *fargs, **fkeywords):
-        # newkeywords = {**keywords, **fkeywords}
-        newkeywords = keywords.copy()
-        newkeywords.update(fkeywords)
-        return func(value, *args, *fargs, **newkeywords)
-    newfunc.func = func
-    newfunc.args = args
-    newfunc.keywords = keywords
-    return newfunc
-
-
 def reduce_helper(value, f, *a, **k):
     """
     Help in `reduce`.

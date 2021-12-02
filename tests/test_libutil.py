bb0772da557ecbe8a490c9b1a0fca0551d3dbcb6
joaomcteixeira <joaomcteixeira@gmail.com>
2021-12-02 11:16:42 +0100

    solves input param in topoaa

--- tests/test_libutil.py
+++ tests/test_libutil.py
@@ -3,7 +3,12 @@ from pathlib import Path
 
 import pytest
 
-from haddock.libs.libutil import file_exists, non_negative_int
+from haddock.libs.libutil import (
+    file_exists,
+    get_number_from_path_stem,
+    non_negative_int,
+    sort_numbered_paths,
+    )
 
 
 @pytest.mark.parametrize(
@@ -52,3 +57,54 @@ def test_file_exists_wrong(i):
     """."""
     with pytest.raises(ValueError):
         file_exists(i)
+
+
+@pytest.mark.parametrize(
+    'in1,expected',
+    [
+        ('pdb_1.pdb', 1),
+        ('pdb2.pdb', 2),
+        ('pdb2.pdb', 2),
+        ('pdb_3.pdb', 3),
+        ('pdb_1231.pdb', 1231),
+        ('pdb_0011.pdb', 11),
+        ('pdb_1_.pdb', 1),
+        ('pdb_1', 1),
+        ('5', 5),
+        ('pdb_20200101_1.pdb', 1),
+        ],
+    )
+def test_get_number(in1, expected):
+    """Test get number from path."""
+    result = get_number_from_path_stem(in1)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'in1,expected',
+    [
+        (
+            ['f_1.pdb', 'f_11.pdb', 'f_2.pdb'],
+            ['f_1.pdb', 'f_2.pdb', 'f_11.pdb'],
+            ),
+        (
+            ['b.pdb', 'c.pdb', 'a.pdb'],
+            ['a.pdb', 'b.pdb', 'c.pdb']),
+        ],
+    )
+def test_sort_numbered_input_1(in1, expected):
+    """Test sort numbered inputs."""
+    result = sort_numbered_paths(*in1)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    'in1,error',
+    [
+        (['f_1.pdb', 'f_11.pdb', 'f_2.pdb'], TypeError),
+        ]
+    )
+def test_sort_numbered_inputs_error(in1, error):
+    """Test sort numbered inputs raised Errors."""
+    with pytest.raises(error):
+        sort_numbered_paths(in1)

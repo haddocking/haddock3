18eeee59eed4e7a2f15ba93d54ea22c547dafdc6
mgiulini <mgiulini@tintin.cluster>
2022-04-08 14:22:12 +0200

    modified log level from info to debug for core-specific calculations

--- src/haddock/libs/libsubprocess.py
+++ src/haddock/libs/libsubprocess.py
@@ -81,32 +81,32 @@ class CapriJob:
         
         # running calculations
         if self.params["fnat"]:
-            log.info(f"core {self.capri_obj.core}, calculating FNAT")
+            log.debug(f"core {self.capri_obj.core}, calculating FNAT")
             fnat_cutoff = self.params["fnat_cutoff"]
-            log.info(f" cutoff: {fnat_cutoff}A")
+            log.debug(f" cutoff: {fnat_cutoff}A")
             self.capri_obj.fnat(cutoff=fnat_cutoff)
 
         if self.params["irmsd"]:
-            log.info(f"core {self.capri_obj.core}, calculating I-RMSD")
+            log.debug(f"core {self.capri_obj.core}, calculating I-RMSD")
             irmsd_cutoff = self.params["irmsd_cutoff"]
-            log.info(f" cutoff: {irmsd_cutoff}A")
+            log.debug(f" cutoff: {irmsd_cutoff}A")
             self.capri_obj.irmsd(cutoff=irmsd_cutoff)
 
         if self.params["lrmsd"]:
-            log.info(f"core {self.capri_obj.core}, calculating L-RMSD")
+            log.debug(f"core {self.capri_obj.core}, calculating L-RMSD")
             self.capri_obj.lrmsd()
 
         if self.params["ilrmsd"]:
-            log.info(f"core {self.capri_obj.core}, calculating I-L-RMSD")
+            log.debug(f"core {self.capri_obj.core}, calculating I-L-RMSD")
             ilrmsd_cutoff = self.params["irmsd_cutoff"]
-            log.info(f" cutoff: {ilrmsd_cutoff}A")
+            log.debug(f" cutoff: {ilrmsd_cutoff}A")
 
             self.capri_obj.ilrmsd(
                 cutoff=ilrmsd_cutoff,
                 )
 
         if self.params["dockq"]:
-            log.info(f"core {self.capri_obj.core}, calculating DockQ metric")
+            log.debug(f"core {self.capri_obj.core}, calculating DockQ metric")
             self.capri_obj.dockq()
         
         self.capri_obj.output(

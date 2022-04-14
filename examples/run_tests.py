13105f2547974c22131c56ea15ad57768cd6ddb8
mgiulini <mgiulini@tintin.cluster>
2022-04-14 16:28:39 +0200

    adjusted run_tests

--- examples/run_tests.py
+++ examples/run_tests.py
@@ -43,8 +43,6 @@ except Exception:
 # the whitespaces below are anti-pythonic but facilitate reading :-)
 examples = (
     ("docking-antibody-antigen"    , "docking-antibody-antigen-ranairCDR-test.cfg"),  # noqa: E203, E501
-    ("docking-antibody-antigen"    , "docking-antibody-antigen-ranairCDR-ems-test.cfg"),  # noqa: E203, E501
-    ("docking-antibody-antigen"    , "docking-antibody-antigen-ranairCDR-mds-test.cfg"),  # noqa: E203, E501
     ("docking-protein-DNA"         , "docking-protein-DNA-test.cfg"),  # noqa: E203, E501
     ("docking-protein-DNA"         , "docking-protein-DNA-mdref-test.cfg"),  # noqa: E203, E501
     ("docking-protein-homotrimer"  , "docking-protein-homotrimer-test.cfg"),  # noqa: E203, E501
@@ -58,6 +56,7 @@ examples = (
     ("refine-complex"              , "refine-complex-test.cfg"),  # noqa: E203, E501
     ("scoring"                     , "emscoring-test.cfg"),  # noqa: E203, E501
     ("scoring"                     , "mdscoring-test.cfg"),  # noqa: E203, E501
+    ("scoring"                     , "emscoring-mdscoring-test.cfg")  # noqa: E203, E501
     )
 
 

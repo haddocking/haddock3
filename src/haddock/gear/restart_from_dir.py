6808a70e90abab9eca04c2cc2f16868d498ea446
joaomcteixeira <joaomcteixeira@gmail.com>
2022-05-03 14:17:36 +0200

    add missing gear file

---
+++ src/haddock/gear/restart_from_dir.py
@@ -0,0 +1,47 @@
+"""Restart from directory gear."""
+from pathlib import Path
+
+from haddock.core.defaults import MODULE_IO_FILE, modules_folder_prefix
+from haddock.libs.libontology import ModuleIO
+
+
+RESTART_FROM_DIR_DEFAULT = None
+
+
+def add_restart_from_dir(parser):
+    """Add option to restart-from-dir."""
+    parser.add_argument(
+        '--restart-from-dir',
+        help="The run directory to restart from.",
+        default=RESTART_FROM_DIR_DEFAULT,
+        type=Path,
+        )
+
+
+def read_num_molecules_from_folder(folder):
+    """
+    Read the number of molecules from the first step folder.
+
+    1. Find the lower indexed step folder in `folder`.
+    2. Read the `io.json` file.
+    3. Count the number of items in the "output" key.
+    4. The above is the number of molecules.
+
+    Parameters
+    ----------
+    folder : Path or string
+        The run directory.
+
+    Returns
+    -------
+    int
+        The number of molecules found.
+    """
+    previous = list(Path(folder).resolve().glob(modules_folder_prefix))
+    previous.sort()
+
+    io = ModuleIO()
+    previous_io = Path(previous[0], MODULE_IO_FILE)
+    io.load(previous_io)
+
+    return len(io.output)

44ed3248f23287e256d4b8cf61c2a84bb5c3e852
rodrigo <r.vargashonorato@uu.nl>
2022-05-30 10:24:54 +0200

    refactor `caprieval`

--- src/haddock/libs/libio.py
+++ src/haddock/libs/libio.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 import yaml
 
+from haddock.libs.libontology import PDBFile
+
 
 def read_from_yaml(yaml_file):
     """
@@ -33,6 +35,101 @@ def read_from_yaml(yaml_file):
 
     assert isinstance(ycfg, dict), type(ycfg)
     return ycfg
+
+
+def write_dic_to_file(
+        data_dict,
+        output_fname,
+        info_header="",
+        sep="\t",
+        ):
+    """
+    Write a dictionary to a file.
+
+    Parameters
+    ----------
+    data_dict : dict
+        Dictionary to write.
+    output_fname : str or Path
+        Name of the output file.
+    info_header : str
+        Header to write before the data.
+    """
+    with open(output_fname, 'w') as fout:
+        fout.write(info_header)
+        fout.write(str(data_dict))
+
+    header = "\t".join(data_dict.keys())
+
+    if info_header:
+        header = info_header + os.linesep + header
+
+    with open(output_fname, "w") as out_fh:
+        out_fh.write(header + os.linesep)
+        row_l = []
+        for element in data_dict:
+            value = data_dict[element]
+            if isinstance(value, Path):
+                row_l.append(str(value))
+            elif isinstance(value, PDBFile):
+                row_l.append(str(value.rel_path))
+            elif isinstance(value, int):
+                row_l.append(f"{value}")
+            elif isinstance(value, str):
+                row_l.append(f"{value}")
+            elif value is None:
+                row_l.append("-")
+            else:
+                row_l.append(f"{value:.3f}")
+        out_fh.write(sep.join(row_l) + os.linesep)
+
+
+def write_nested_dic_to_file(
+        data_dict,
+        output_fname,
+        info_header="",
+        sep="\t"
+        ):
+    """
+    Write a dictionary to a file.
+
+    Parameters
+    ----------
+    data_dict : dict
+        Dictionary to write.
+    output_fname : str or Path
+        Name of the output file.
+
+    Notes
+    -----
+    This function is used to write nested dictionaries.
+    {int: {key: value}}, the int key will be discarded.
+    """
+    first_key = list(data_dict.keys())[0]
+    header = "\t".join(data_dict[first_key].keys())
+
+    if info_header:
+        header = info_header + os.linesep + header
+
+    with open(output_fname, "w") as out_fh:
+        out_fh.write(header + os.linesep)
+        for row in data_dict:
+            row_l = []
+            for element in data_dict[row]:
+                value = data_dict[row][element]
+                if isinstance(value, Path):
+                    row_l.append(str(value))
+                elif isinstance(value, PDBFile):
+                    row_l.append(str(value.rel_path))
+                elif isinstance(value, int):
+                    row_l.append(f"{value}")
+                elif isinstance(value, str):
+                    row_l.append(f"{value}")
+                elif value is None:
+                    row_l.append("-")
+                else:
+                    row_l.append(f"{value:.3f}")
+            out_fh.write(sep.join(row_l) + os.linesep)
 
 
 # thanks to @brianjimenez
--- tests/test_libio.py
+++ tests/test_libio.py
@@ -1,7 +1,14 @@
 """Test libio."""
+import tempfile
+from pathlib import Path
+
 import pytest
 
-from haddock.libs.libio import read_from_yaml
+from haddock.libs.libio import (
+    read_from_yaml,
+    write_dic_to_file,
+    write_nested_dic_to_file,
+    )
 
 from . import emptycfg, haddock3_yaml_cfg_examples
 
@@ -17,3 +24,29 @@ def test_read_from_yaml(cfg):
     """Test read from yaml file."""
     result = read_from_yaml(cfg)
     assert isinstance(result, dict)
+
+
+def test_write_nested_dic_to_file():
+    """Test write nested dictionary to file."""
+    f = tempfile.NamedTemporaryFile(delete=False)
+    write_nested_dic_to_file(
+        data_dict={1: {"something": "something"}},
+        output_fname=f.name)
+
+    assert Path(f.name).exists()
+    assert Path(f.name).stat().st_size != 0
+
+    Path(f.name).unlink()
+
+
+def test_write_dic_to_file():
+    """Test write dictionary to file."""
+    f = tempfile.NamedTemporaryFile(delete=False)
+    write_dic_to_file(
+        data_dict={"something": "something"},
+        output_fname=f.name)
+
+    assert Path(f.name).exists()
+    assert Path(f.name).stat().st_size != 0
+
+    Path(f.name).unlink()

3bf5be7dc1bd65c6a58b286992a315d22937d1d0
joaomcteixeira <joaomcteixeira@gmail.com>
2022-06-22 12:47:16 +0200

    with multiple params

--- src/haddock/clis/cli_cfg.py
+++ src/haddock/clis/cli_cfg.py
@@ -10,6 +10,7 @@ Usage::
     haddock3-cfg -m MODULE -l LEVEL
     haddock3-cfg -m topoaa -l all
     haddock3-cfg -m rigidbody -l all -g
+    haddock3-cfg -G
 """
 import argparse
 import importlib
@@ -26,15 +27,36 @@ ap = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
     )
 
-ap.add_argument(
+_group = ap.add_argument_group(
+    "Mandatory parameters",
+    "These parameters are mutually exclusive.",
+    )
+group = _group.add_mutually_exclusive_group(required=True)
+groupm = ap.add_argument_group(
+    title="Additional arguments when selecting a module",
+    description=(
+        "Use these optional arguments together with the `-m` option. "
+        "If `-G` is given, these arguments will be ignored."
+        ),
+    )
+
+group.add_argument(
     "-m",
     dest="module",
     help="The module for which you want to retrieve the default configuration.",
-    required=True,
     choices=sorted(modules_category.keys()),
+    default=None,
     )
 
-ap.add_argument(
+group.add_argument(
+    "-G",
+    "--only-globals",
+    dest="only_globals",
+    help="Retrieve only the optional module's general parameters.",
+    action="store_true",
+    )
+
+groupm.add_argument(
     "-l",
     dest="explevel",
     required=False,
@@ -43,7 +65,7 @@ ap.add_argument(
     choices=config_expert_levels + ("all",),
     )
 
-ap.add_argument(
+groupm.add_argument(
     '-g',
     dest='add_global',
     help="Add also the optional module's general parameters.",
@@ -73,26 +95,33 @@ def maincli():
     cli(ap, main)
 
 
-def main(module, explevel, add_global=True):
-    """Extract the default configuration file for a given module."""
+def main(module=None, explevel="all", add_globals=True):
+    """
+    Extract the defaults in the form of a run configuration file.
+
+    Parameters
+    ----------
+    module : str or None
+        The module name to extract the defaults from. ``None`` can be given
+        if ``only_globals`` is ``True``.
+
+    explevel : str
+        Filter the parameters according to the expert level. Output all
+        parameters that belong to the referred level or below. Choices
+        are: all, easy, expert, and guru.
+
+    add_globals : bool
+        Whether to add the module's general global parameter. If this
+        option is given and ``module`` is ``None``, outputs only the
+        general parameters.
+    """
     from haddock import modules_defaults_path
     from haddock.gear.yaml2cfg import yaml2cfg_text
     from haddock.libs.libio import read_from_yaml
 
-    module_name = ".".join((
-        'haddock',
-        'modules',
-        modules_category[module],
-        module,
-        ))
+    new_config = ''
 
-    module_lib = importlib.import_module(module_name)
-    cfg = module_lib.DEFAULT_CONFIG
-
-    ycfg = read_from_yaml(cfg)
-    new_config = yaml2cfg_text(ycfg, module, explevel)
-
-    if add_global:
+    if add_globals:
         general_cfg = read_from_yaml(modules_defaults_path)
         general_params_str = yaml2cfg_text(
             general_cfg,
@@ -108,10 +137,26 @@ def main(module, explevel, add_global=True):
         new_config = os.linesep.join((
             comment,
             general_params_str,
-            new_config,
             ))
 
-    print(new_config, file=sys.stdout, flush=True)  # noqa: T201
+    if module:
+
+        module_name = ".".join((
+            'haddock',
+            'modules',
+            modules_category[module],
+            module,
+            ))
+
+        module_lib = importlib.import_module(module_name)
+        cfg = module_lib.DEFAULT_CONFIG
+
+        ycfg = read_from_yaml(cfg)
+        module_config = yaml2cfg_text(ycfg, module, explevel)
+
+        new_config = os.linesep.join((new_config, module_config))
+
+    sys.stdout.write(new_config, flush=True)
 
     return 0
 

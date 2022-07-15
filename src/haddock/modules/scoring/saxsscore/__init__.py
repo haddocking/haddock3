218113631e017b1154f86b1d52a1afcb0f05950a
Charlotte van Noort <61543485+cvnoort@users.noreply.github.com>
2022-07-15 13:33:38 +0200

    Update SAXS scoring module
    Opening output file only once to write HADDOCKsaxs scores.

--- src/haddock/modules/scoring/saxsscore/__init__.py
+++ src/haddock/modules/scoring/saxsscore/__init__.py
@@ -37,39 +37,43 @@ class HaddockModule(BaseHaddockModule):
         ns = self.params["ns"]
         cst = self.params["cst"]
 
-        # Get the weights from the defaults
+        # Get the weights
         _weight_keys = ("w_vdw", "w_elec", "w_desolv", "w_air", "w_bsa")
         weights = {e: self.params[e] for e in _weight_keys}
 
-        # Initiate output file
-        output_fname = "saxsscore.tsv"
-        with open(output_fname, "w") as fh:
-            fh.write("\t".join(("structure","chi","score")) + linesep)
+        for model_num, model in enumerate(models_to_score, start=1):
 
-        with open(output_fname, "a") as fh:
-            for model_num, model in enumerate(models_to_score, start=1):
+            # Calculate chi value
+            cmd = f"crysol {model.rel_path} {saxs_data} -lm {lm} -ns {ns}"
+            if cst:
+                cmd += " -cst"
+            subprocess.call(cmd, shell=True)
 
-                # Calculate chi value
-                cmd = f"crysol {model.rel_path} {saxs_data} -lm {lm} -ns {ns}"
-                if cst:
-                    cmd += " -cst"
-                subprocess.call(cmd, shell=True)
+            # Read chi value
+            fit_file = model.file_name.replace(".pdb","00.fit")
+            with open(fit_file, "r") as ff:
+                fit_header = ff.readline()
+                model.chi = float(fit_header[fit_header.rfind("Chi^2:")+6:])
 
-                # Read chi value
-                fit_file = model.file_name.replace(".pdb","00.fit")
-                with open(fit_file, "r") as ff:
-                    fit_header = ff.readline()
-                    chi = float(fit_header[fit_header.rfind("Chi^2:")+6:])
+            # Calculate HADDOCKsaxs score
+            haddock_score = HaddockModel(model.rel_path).calc_haddock_score(
+                **weights ) + ( model.chi * self.params["w_saxs"] )
 
-                # Calculate HADDOCKsaxs score
-                haddock_score = HaddockModel(model.rel_path).calc_haddock_score(
-                    **weights ) + ( chi * self.params["w_saxs"] )
+            model.score = haddock_score
 
-                model.score = haddock_score
-
-                # Write score to output file
-                fh.write(f"{model.file_name}\t{chi}\t{haddock_score}" + linesep)
-
-        # Send models (unchanged) to the next step
         self.output_models = models_to_score
+
+        # Write score to output file
+        output_fname = "saxsscore.tsv"
+        self.log(f"Saving output to {output_fname}")
+
+        text_generator = (
+            f"{pdb.file_name}\t{pdb.ori_name}\t{pdb.md5}\t{pdb.chi}\t{pdb.score}"
+            for pdb in self.output_models )
+
+        with open(output_fname, "w") as fh:
+            fh.write("\t".join("structure","original_name","md5","chi","score") + linesep)
+            fh.write(linesep.join(text_generator))
+
+        # Send models (unchanged but with HADDOCKsaxs score) to the next step
         self.export_output_models()

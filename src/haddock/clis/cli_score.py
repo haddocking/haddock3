1634e5edade2aa5f6bb51d3be43b3bdf8491f258
joaomcteixeira <joaomcteixeira@gmail.com>
2022-08-16 11:19:36 +0200

    completes the client and solves some issues

--- src/haddock/clis/cli_score.py
+++ src/haddock/clis/cli_score.py
@@ -1,8 +1,20 @@
 """
 A simple tool to calculate the HADDOCK-score of a complex.
 
-Usage:
+You can pass to the command-line any parameter accepted by the `emscore` module.
+For this, use the ``-p`` option writing the name of the parameters followed by
+the desired value. Write booleans with capital letter.
+
+Use the ``haddock3-cfg`` command-line to obtain the list of parameters for
+the ``emscore`` module.
+
+Usage::
+
     haddock3-score complex.pdb
+    haddock3-score complex.pdb -p nemsteps 50
+    haddock3-score complex.pdb -p nemsteps 50 w_air 1
+    haddock3-score complex.pdb -p nemsteps 50 w_air 1 electflag True
+
 """
 import argparse
 import ast
@@ -19,7 +31,7 @@ class _ParamsToDict(argparse.Action):
     Where ``-x`` is an optional argument of the command-line client
     interface.
 
-        >>> par1 1 par2 'my name' par3 [1,2,3]
+        >>> par1 1 par2 'my name' par3 [1,2,3] par4 True
         >>> {'par1': 1, 'par2': 'my name', 'par3': [1, 2, 3]}
 
     """
@@ -81,12 +93,14 @@ ap.add_argument(
 ap.add_argument(
     "-p",
     "--other-params",
+    dest="other_params",
     help=(
         "Any other parameter of the `emscoring` module."
         "For example: -p nemsteps 1000. You can give any number of "
         "parameters."
         ),
     action=_ParamsToDict,
+    default={},
     nargs="*",
     )
 
@@ -120,31 +134,72 @@ def main(
         keep_all=False,
         **kwargs,
         ):
-    """Calculate the score of a complex."""
+    """
+    Calculate the score of a complex using the ``emscoring`` module.
+
+    Parameters
+    ----------
+    pdb_file : str or pathlib.Path
+        The path to the PDB containing the complex.
+
+    full : bool
+        Print all energy components.
+
+    outputpdb : bool
+        Save the PDB file resulting from the scoring calculation.
+
+    outputpsf : bool
+        Save  the PSF file resulting from the scoring calculation; this
+        is the CNS topology created before running the calculation.
+
+    keep_all : bool
+        Keep the whole temporary run folder. ``haddock3-score`` creates
+        a temporary run folder where the calculations are performed. If
+        ``keep_all`` is True, this folder is **not** deleted after when
+        the calculation finishes.
+
+    kwargs : any
+        Any additional arguments that will be passed to the ``emscoring``
+        module.
+    """
     import logging
     import shutil
+    from contextlib import suppress
     from pathlib import Path
 
     from haddock import log
     from haddock.gear.haddockmodel import HaddockModel
+    from haddock.gear.yaml2cfg import read_from_yaml_config
     from haddock.gear.zerofill import zero_fill
     from haddock.libs.libio import working_directory
     from haddock.libs.libworkflow import WorkflowManager
+    from haddock.modules.scoring.emscoring import DEFAULT_CONFIG
 
     log.setLevel(logging.ERROR)
 
     input_pdb = Path(pdb_file).resolve()
+    if not input_pdb.exists():
+        sys.exit(f'* ERROR * Input PDB file {str(input_pdb)!r} does not exist')
 
-    run_dir = Path("haddock-score-client")
-    run_dir.mkdir()
-    zero_fill.set_zerofill_number(2)
+    # config all parameters are correctly spelled.
+    default_emscoring = read_from_yaml_config(DEFAULT_CONFIG)
+    for param in kwargs:
+        if param not in default_emscoring:
+            sys.exit(f'* ERROR * Parameter {param!r} is not a valid `emscoring` parameter')  # noqa:E501
 
     params = {
         "topoaa": {"molecules": [input_pdb]},
         "emscoring": kwargs,
         }
 
-    print("> starting calculations...")  # noqa: T201
+    print("> starting calculations...")
+
+    run_dir = Path("haddock-score-client")
+    with suppress(FileNotFoundError):
+        shutil.rmtree(run_dir)
+    run_dir.mkdir()
+    zero_fill.set_zerofill_number(2)
+
     with working_directory(run_dir):
         workflow = WorkflowManager(
             workflow_params=params,
@@ -161,7 +216,7 @@ def main(
     elec = haddock_score_component_dic["elec"]
     desolv = haddock_score_component_dic["desolv"]
     air = haddock_score_component_dic["air"]
-    # bsa = haddock_score_component_dic["bsa"]
+    bsa = haddock_score_component_dic["bsa"]
 
     # emscoring is equivalent to itw
     haddock_score_itw = \
@@ -170,12 +225,15 @@ def main(
         + 1.0 * desolv \
         + 0.1 * air
 
-    print("> HADDOCK-score = (1.0 * vdw) + (0.2 * elec) + (1.0 * desolv) + (0.1 * air)")  # noqa: T201, E501
-    print(f"> HADDOCK-score (emscoring) = {haddock_score_itw:.4f}")  # noqa: T201, E501
+    print("> HADDOCK-score = (1.0 * vdw) + (0.2 * elec) + (1.0 * desolv) + (0.1 * air)")  # noqa: E501
+    print(f"> HADDOCK-score (emscoring) = {haddock_score_itw:.4f}")
+
+    if full:
+        print(f"> vdw={vdw},elec={elec},desolv={desolv},air={air},bsa={bsa}")
 
     if outputpdb:
         outputpdb_name = Path(f"{input_pdb.name}_hs.pdb")
-        print(f"> writing {outputpdb_name}")  # noqa: T201
+        print(f"> writing {outputpdb_name}")
         shutil.copy(
             Path(run_dir, "1_emscoring", "emscoring_1.pdb"),
             outputpdb_name,
@@ -183,7 +241,7 @@ def main(
 
     if outputpsf:
         outputpsf_name = Path(f"{input_pdb.name}_hs.psf")
-        print(f"> writing {outputpsf_name}")  # noqa: T201
+        print(f"> writing {outputpsf_name}")
         shutil.copy(
             Path(run_dir, "0_topoaa", f'{input_pdb.name}_haddock.psf'),
             outputpsf_name,
@@ -191,6 +249,11 @@ def main(
 
     if not keep_all:
         shutil.rmtree(run_dir)
+    else:
+        print(
+            'The folder where the calculations where performed was kept. See '
+            'folder: haddock-scoring-client'
+            )
 
 
 if __name__ == "__main__":

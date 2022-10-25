ddc4315c807203e173df17cc18bb98c2a18aab6f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 09:30:08 +0000

    Adding tarfile member sanitization to extractall()

--- src/haddock/gear/clean_steps.py
+++ src/haddock/gear/clean_steps.py
@@ -117,7 +117,29 @@ def unpack_compressed_and_archived_files(folders, ncores=1):
 
         for tar_file in tar_files:
             with tarfile.open(tar_file) as fin:
-                fin.extractall(folder)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(fin, folder)
 
             tar_file.unlink()
 

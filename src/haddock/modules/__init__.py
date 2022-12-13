9fce05543b9a18e83a0c6bdf260140aa006ce550
joaomcteixeira <joaomcteixeira@gmail.com>
2022-12-13 18:28:50 +0100

    implement max cpus

--- src/haddock/modules/__init__.py
+++ src/haddock/modules/__init__.py
@@ -359,6 +359,7 @@ def get_engine(mode, params):
         return partial(
             Scheduler,
             ncores=params['ncores'],
+            max_ncores=params['max_cpus'],
             )
     elif mode == "mpi":
         return partial(MPIScheduler, ncores=params["ncores"])

97a2746a3ca49127820bb695e9f150a92e4a104f
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2023-03-10 11:54:12 +0100

    Add tests for libplots/find_best_struct

---
+++ tests/test_libplots.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+
+import pytest
+import pandas as pd
+
+from haddock.libs.libplots import find_best_struct
+from . import golden_data, data_folder
+
+
+@pytest.fixture
+def example_capri_ss():
+    """Provide example capri_ss.tsv filename."""
+    return Path(golden_data, "capri_ss_example.tsv")
+
+
+def test_find_best_struct_best1(example_capri_ss):
+    result = find_best_struct(example_capri_ss, 1)
+    cluster1 = result.loc[result['Cluster ID'] == 1]
+
+    expected = pd.DataFrame({
+        'Cluster ID': {
+            0: 1
+        },
+        'Nr 1 best structure': {
+            0: '../1_rigidbody/rigidbody_383.pdb',
+        }
+    })
+    expected.columns.names = ['Structure']
+    pd.testing.assert_frame_equal(cluster1, expected)
+
+
+def test_find_best_struct_best20(example_capri_ss):
+    result = find_best_struct(example_capri_ss, 20)
+    cluster1 = result.loc[result['Cluster ID'] == 1]
+
+    expected = pd.DataFrame({
+        'Cluster ID': {
+            0: 1
+        },
+        'Nr 1 best structure': {
+            0: '../1_rigidbody/rigidbody_383.pdb'
+        },
+        'Nr 2 best structure': {
+            0: '../1_rigidbody/rigidbody_265.pdb'
+        },
+        'Nr 3 best structure': {
+            0: '../1_rigidbody/rigidbody_231.pdb'
+        },
+        'Nr 4 best structure': {
+            0: '../1_rigidbody/rigidbody_218.pdb'
+        }
+    })
+    expected.columns.names = ['Structure']
+    pd.testing.assert_frame_equal(cluster1, expected)
+
+def test_find_best_struct_best10(example_capri_ss):
+    result = find_best_struct(example_capri_ss)
+    cluster1 = result.loc[result['Cluster ID'] == 1]
+
+    expected = pd.DataFrame({
+        'Cluster ID': {
+            0: 1
+        },
+        'Nr 1 best structure': {
+            0: '../1_rigidbody/rigidbody_383.pdb'
+        },
+        'Nr 2 best structure': {
+            0: '../1_rigidbody/rigidbody_265.pdb'
+        },
+        'Nr 3 best structure': {
+            0: '../1_rigidbody/rigidbody_231.pdb'
+        },
+        'Nr 4 best structure': {
+            0: '../1_rigidbody/rigidbody_218.pdb'
+        }
+    })
+    expected.columns.names = ['Structure']
+    pd.testing.assert_frame_equal(cluster1, expected)
+
+
+@pytest.fixture
+def example_capri_ss_dashcluster():
+    """Provide example capri_ss.tsv filename."""
+    return Path(data_folder, "capri_ss_-cluster.tsv")
+
+
+def test_find_best_struct_dashcluster(example_capri_ss_dashcluster):
+    result = find_best_struct(example_capri_ss_dashcluster, 10)
+
+    expected = pd.DataFrame({
+        'Cluster ID': {
+            0: '-'
+        },
+        'Nr 1 best structure': {
+            0: '../../01_rigidbody/rigidbody_6.pdb'
+        },
+        'Nr 2 best structure': {
+            0: '../../01_rigidbody/rigidbody_16.pdb'
+        },
+        'Nr 3 best structure': {
+            0: '../../01_rigidbody/rigidbody_20.pdb'
+        },
+        'Nr 4 best structure': {
+            0: '../../01_rigidbody/rigidbody_14.pdb'
+        }
+    })
+    expected.columns.names = ['Structure']
+    pd.testing.assert_frame_equal(result, expected)

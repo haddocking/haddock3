96e6952cff84af678648bb9e783f7191f5ac84ae
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-03-22 17:33:39 +0100

    add docstrings to test_libplots, fix linter errors

--- tests/test_libplots.py
+++ tests/test_libplots.py
@@ -1,10 +1,12 @@
+"""Test finding the best structures in libplots."""
 from pathlib import Path
 
-import pytest
 import pandas as pd
+import pytest
 
 from haddock.libs.libplots import find_best_struct
-from . import golden_data, data_folder
+
+from . import data_folder, golden_data
 
 
 @pytest.fixture
@@ -14,6 +16,7 @@ def example_capri_ss():
 
 
 def test_find_best_struct_best1(example_capri_ss):
+    """Finds one best structure."""
     result = find_best_struct(example_capri_ss, 1)
     cluster1 = result.loc[result["Cluster ID"] == 1]
 
@@ -22,14 +25,15 @@ def test_find_best_struct_best1(example_capri_ss):
             "Cluster ID": {0: 1},
             "Nr 01 best structure": {
                 0: "../1_rigidbody/rigidbody_383.pdb",
-            },
-        }
-    )
+                },
+            }
+        )
     expected.columns.names = ["Structure"]
     pd.testing.assert_frame_equal(cluster1, expected)
 
 
 def test_find_best_struct_best20(example_capri_ss):
+    """Finds 20 best structures if possible."""
     result = find_best_struct(example_capri_ss, 20)
     cluster1 = result.loc[result["Cluster ID"] == 1]
 
@@ -40,13 +44,14 @@ def test_find_best_struct_best20(example_capri_ss):
             "Nr 02 best structure": {0: "../1_rigidbody/rigidbody_265.pdb"},
             "Nr 03 best structure": {0: "../1_rigidbody/rigidbody_231.pdb"},
             "Nr 04 best structure": {0: "../1_rigidbody/rigidbody_218.pdb"},
-        }
-    )
+            }
+        )
     expected.columns.names = ["Structure"]
     pd.testing.assert_frame_equal(cluster1, expected)
 
 
-def test_find_best_struct_best10(example_capri_ss):
+def test_find_best_struct_best4(example_capri_ss):
+    """Finds 4 best structures if possible."""
     result = find_best_struct(example_capri_ss, 4)
     cluster1 = result.loc[result["Cluster ID"] == 1]
 
@@ -57,8 +62,8 @@ def test_find_best_struct_best10(example_capri_ss):
             "Nr 02 best structure": {0: "../1_rigidbody/rigidbody_265.pdb"},
             "Nr 03 best structure": {0: "../1_rigidbody/rigidbody_231.pdb"},
             "Nr 04 best structure": {0: "../1_rigidbody/rigidbody_218.pdb"},
-        }
-    )
+            }
+        )
     expected.columns.names = ["Structure"]
     pd.testing.assert_frame_equal(cluster1, expected)
 
@@ -70,6 +75,7 @@ def example_capri_ss_dashcluster():
 
 
 def test_find_best_struct_unclustered(example_capri_ss_dashcluster):
+    """Finds 4 best structures when there unclustered."""
     result = find_best_struct(example_capri_ss_dashcluster, 4)
 
     expected = pd.DataFrame(
@@ -79,7 +85,7 @@ def test_find_best_struct_unclustered(example_capri_ss_dashcluster):
             "Nr 02 best structure": {0: "../../01_rigidbody/rigidbody_16.pdb"},
             "Nr 03 best structure": {0: "../../01_rigidbody/rigidbody_20.pdb"},
             "Nr 04 best structure": {0: "../../01_rigidbody/rigidbody_14.pdb"},
-        }
-    )
+            }
+        )
     expected.columns.names = ["Structure"]
     pd.testing.assert_frame_equal(result, expected)

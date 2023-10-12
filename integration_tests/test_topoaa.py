de7d7a388ee62b4e87bfb4e0ca747b6a54bf2eef
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2023-10-12 14:07:42 +0200

    add test_topoaa

---
+++ integration_tests/test_topoaa.py
@@ -0,0 +1,68 @@
+import os
+import tempfile
+from pathlib import Path
+
+import pytest
+
+from haddock.modules.topology.topoaa import DEFAULT_CONFIG as DEFAULT_TOPOAA_CONFIG
+from haddock.modules.topology.topoaa import HaddockModule as TopoaaModule
+
+if "CNS_EXEC" in os.environ:
+    CNS_EXEC = os.environ["CNS_EXEC"]
+else:
+    raise ValueError("CNS_EXEC not found in the environment")
+
+DATA_DIR = Path(Path(__file__).parent.parent / "examples")
+
+
+@pytest.fixture
+def topoaa_module():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        topoaa = TopoaaModule(
+            order=0, path=tmpdir, initial_params=DEFAULT_TOPOAA_CONFIG
+        )
+        topoaa.__init__(path=tmpdir, order=0)
+        topoaa.params["molecules"] = [
+            Path(DATA_DIR, "docking-protein-protein/data/e2aP_1F3G.pdb"),
+            Path(DATA_DIR, "docking-protein-protein/data/hpr_ensemble.pdb"),
+        ]
+        topoaa.params["mol1"] = {"prot_segid": "A"}
+        topoaa.params["mol2"] = {"prot_segid": "B"}
+
+        topoaa.params["cns_exec"] = CNS_EXEC
+
+        yield topoaa
+
+
+def test_topoaa_default(topoaa_module):
+    """Test the topoaa module."""
+
+    topoaa_module.run()
+
+    expected_inp = Path(topoaa_module.path, "e2aP_1F3G.inp")
+    expected_psf = Path(topoaa_module.path, "e2aP_1F3G_haddock.psf")
+    expected_pdb = Path(topoaa_module.path, "e2aP_1F3G_haddock.pdb")
+    expected_gz = Path(topoaa_module.path, "e2aP_1F3G.out.gz")
+
+    assert expected_inp.exists(), f"{expected_inp} does not exist"
+    assert expected_psf.exists(), f"{expected_psf} does not exist"
+    assert expected_gz.exists(), f"{expected_gz} does not exist"
+    assert expected_pdb.exists(), f"{expected_pdb} does not exist"
+
+    assert expected_inp.stat().st_size > 0, f"{expected_inp} is empty"
+
+    for i in range(1, 10 + 1):
+        expected_inp = Path(topoaa_module.path, f"hpr_ensemble_{i}.inp")
+        expected_psf = Path(topoaa_module.path, f"hpr_ensemble_{i}_haddock.psf")
+        expected_pdb = Path(topoaa_module.path, f"hpr_ensemble_{i}_haddock.pdb")
+        expected_gz = Path(topoaa_module.path, f"hpr_ensemble_{i}.out.gz")
+
+        assert expected_inp.exists(), f"{expected_inp} does not exist"
+        assert expected_psf.exists(), f"{expected_psf} does not exist"
+        assert expected_pdb.exists(), f"{expected_pdb} does not exist"
+        assert expected_gz.exists(), f"{expected_gz} does not exist"
+
+        assert expected_inp.stat().st_size > 0, f"{expected_inp} is empty"
+        assert expected_psf.stat().st_size > 0, f"{expected_psf} is empty"
+        assert expected_pdb.stat().st_size > 0, f"{expected_pdb} is empty"
+        assert expected_gz.stat().st_size > 0, f"{expected_gz} is empty"

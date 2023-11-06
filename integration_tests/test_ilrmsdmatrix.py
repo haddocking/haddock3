8edac039c26d03b476ebe8327cf55ed2f2aad1f9
mgiulini <mrcgiulini@gmail.com>
2023-11-06 13:57:51 +0100

    adjusted integration test

--- integration_tests/test_ilrmsdmatrix.py
+++ integration_tests/test_ilrmsdmatrix.py
@@ -9,8 +9,7 @@ from haddock.libs.libontology import PDBFile
 
 from haddock.modules.analysis.ilrmsdmatrix import DEFAULT_CONFIG as DEFAULT_ILRMSD_CONFIG
 from haddock.modules.analysis.ilrmsdmatrix import HaddockModule as IlrmsdmatrixModule
-DATA_DIR = Path(Path(__file__).parent.parent / "tests" / "golden_data")
-
+from . import golden_data
 
 @pytest.fixture
 def ilrmsdmatrix_module():
@@ -18,8 +17,6 @@ def ilrmsdmatrix_module():
         ilrmsdmatrix = IlrmsdmatrixModule(
             order=0, path=tmpdir, initial_params=DEFAULT_ILRMSD_CONFIG
         )
-        ilrmsdmatrix.params["receptor_chain"] = "R"
-        ilrmsdmatrix.params["ligand_chains"] = ["S"]
         yield ilrmsdmatrix
 
 
@@ -28,12 +25,12 @@ class MockPreviousIO():
         self.path = path
 
     def retrieve_models(self, individualize: bool = False):
-        shutil.copy(Path(DATA_DIR, "protprot_1bkd_1.pdb"), Path(".", "protprot_complex_1.pdb"))
-        shutil.copy(Path(DATA_DIR, "protprot_1bkd_2.pdb"), Path(".", "protprot_complex_2.pdb"))
+        shutil.copy(Path(golden_data, "protglyc_complex_1.pdb"), Path(".", "protglyc_complex_1.pdb"))
+        shutil.copy(Path(golden_data, "protglyc_complex_2.pdb"), Path(".", "protglyc_complex_2.pdb"))
         
         model_list = [
-            PDBFile(file_name="protprot_complex_1.pdb", path="."),
-            PDBFile(file_name="protprot_complex_2.pdb", path="."),
+            PDBFile(file_name="protglyc_complex_1.pdb", path="."),
+            PDBFile(file_name="protglyc_complex_2.pdb", path="."),
         ]
 
         return model_list
@@ -51,8 +48,8 @@ def test_ilrmsdmatrix_default(ilrmsdmatrix_module, mocker):
     assert exp_contacts_file.exists(), "receptor_contacts.con does not exist"
     # open files and check content
     with open(exp_ilrmsd_matrix) as f:
-        assert f.readline() == "1 2 20.020\n"
+        assert f.readline() == "1 2 11.877\n"
     with open(exp_contacts_file) as f:
         lines = f.readlines()
-        assert lines[0] == "R 12 13 15 17 21 25 30 31 32 33 34 35 37 38 39 40 41 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 73 95 102 103 105\n"
-        assert lines[1] == "S 809 810 814 822 824 825 826 828 829 832 833 836 869 872 875 876 879 880 881 882 884 908 909 910 911 912 913 929 930 931 932 934 935 936 938 939 940 942 943 944 945 963 1002 1003 1006 1007 1010 1019\n"
+        assert lines[0] == f"A 35 44 46 48 50 52 56 57 58 59 61 62 63 73 75 98 101 102 103 104 106 107 108 109 110 112{os.linesep}"
+        assert lines[1] == f"B 1 2 3 4{os.linesep}"

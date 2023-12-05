329e1f3a721626beb5fbc78396eb94e012329c2d
mgiulini <mrcgiulini@gmail.com>
2023-12-05 16:52:09 +0100

    added tests

--- tests/test_cli_traceback.py
+++ tests/test_cli_traceback.py
@@ -3,11 +3,12 @@
 import os
 import shutil
 from pathlib import Path
+import tempfile
 
 import pandas as pd
 import pytest
 
-from haddock.clis.cli_traceback import main
+from haddock.clis.cli_traceback import main, get_steps_without_pdbs
 
 from . import golden_data
 
@@ -39,6 +40,12 @@ def test_main(rigid_json, flexref_json):
     shutil.copy(rigid_json, os.path.join(step_dirs[0], "io.json"))
     shutil.copy(flexref_json, os.path.join(step_dirs[1], "io.json"))
 
+    # create fake, empty pdb files
+    for i in range(1, 5):
+        open(os.path.join(step_dirs[0], f"rigidbody_{i}.pdb"), "w").close()
+    for i in range(1, 3):
+        open(os.path.join(step_dirs[1], f"flexref_{i}.pdb"), "w").close()
+    
     # run haddock3-traceback
     main(run_dir)
 
@@ -82,3 +89,33 @@ def test_analysis():
 
     # check traceback folder does not exist
     assert not os.path.isdir(os.path.join(run_dir, "traceback"))
+
+
+def test_get_steps_without_pdbs():
+    """Test get_steps_without_pdbs."""
+    with tempfile.TemporaryDirectory(dir=".") as tmpdir:
+        # build fake run_dir
+        run_dir = Path(tmpdir)
+        steps = ["0_topoaa", "1_rigidbody", "2_caprieval"]
+        
+        for st in steps:
+            os.mkdir(Path(run_dir, st))
+
+        # create fake, empty pdb files
+        fake_pdb_path = Path(run_dir, steps[1] , f"rigidbody_1.pdb")
+        open(fake_pdb_path, "w").close()
+
+        # get steps without pdbs
+        obs_steps = get_steps_without_pdbs(run_dir, steps)
+
+        # check steps without pdbs
+        exp_steps = ["0_topoaa", "2_caprieval"]
+        assert obs_steps == exp_steps
+
+        # now we remove the fake pdb file
+        os.remove(fake_pdb_path)
+
+        # get steps without pdbs
+        obs_steps = get_steps_without_pdbs(run_dir, steps)
+        exp_steps = ["0_topoaa", "1_rigidbody", "2_caprieval"]
+        assert obs_steps == exp_steps

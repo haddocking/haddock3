0d96ba0ecf589be013cbf5702722bbf7e4846df6
mgiulini <mrcgiulini@gmail.com>
2023-12-13 11:23:33 +0100

    added is_cleaned logic

--- src/haddock/clis/cli_analyse.py
+++ src/haddock/clis/cli_analyse.py
@@ -289,6 +289,7 @@ def analyse_step(
     top_cluster: int,
     format: Optional[ImgFormat],
     scale: Optional[float],
+    is_cleaned: Optional[bool],
 ) -> None:
     """
     Analyse a step.
@@ -343,7 +344,7 @@ def analyse_step(
         log.info("Plotting results..")
         scatters = scatter_plot_handler(ss_file, cluster_ranking, format, scale)
         boxes = box_plot_handler(ss_file, cluster_ranking, format, scale)
-        tables = clt_table_handler(clt_file, ss_file)
+        tables = clt_table_handler(clt_file, ss_file, is_cleaned)
         report_generator(boxes, scatters, tables, step)
 
 
@@ -353,6 +354,7 @@ def main(
     top_cluster: int,
     format: Optional[ImgFormat],
     scale: Optional[float],
+    is_cleaned: Optional[bool],
     **kwargs: Any,
 ) -> None:
     """
@@ -424,7 +426,7 @@ def main(
         error = False
         try:
             analyse_step(
-                step, Path("./"), capri_dict, target_path, top_cluster, format, scale
+                step, Path("./"), capri_dict, target_path, top_cluster, format, scale, is_cleaned
             )
         except Exception as e:
             error = True

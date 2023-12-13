4a59b9e437119b7e098571b367bf14bb8be8154e
mgiulini <mrcgiulini@gmail.com>
2023-12-13 14:20:30 +0100

    added test for libworkflow

---
+++ tests/test_libworkflow.py
@@ -0,0 +1,26 @@
+from haddock.libs.libworkflow import WorkflowManager
+from haddock.core.typing import Any, ModuleParams
+import tempfile
+from pathlib import Path
+import os
+
+def test_WorkflowManager(caplog):
+    """Test WorkflowManager."""
+    ParamDict = {
+        'topoaa.1':
+            {'autohis': True,
+             'molecules': ['fake.pdb'],
+             'clean': True
+            }
+        }
+    with tempfile.TemporaryDirectory(dir=".") as tmpdir:
+        workflow = WorkflowManager(
+            ParamDict,
+            start=0,
+            other_params=Any,)
+        workflow.postprocess()
+        first_log_line = str(caplog.records[0].message)
+        second_log_line = str(caplog.records[1].message)
+        assert first_log_line == "Reading instructions step 0_topoaa"
+        assert second_log_line == "Running haddock3-analyse on ./, modules [], with top_cluster = 10"
+        

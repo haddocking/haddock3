f3e7156debf766af7a8d8abd0cb62e4fa1a6fc9c
VGPReys <v.g.p.reys@uu.nl>
2024-03-05 16:29:02 +0100

    reworked seletopclust module

--- src/haddock/modules/analysis/seletopclusts/__init__.py
+++ src/haddock/modules/analysis/seletopclusts/__init__.py
@@ -3,17 +3,21 @@
 This module selects a number of models from a number of clusters. The
 selection is based on the score of the models within the clusters.
 
-In the standard HADDOCK analysis the top 4 models of the top 10 clusters
+In the standard HADDOCK analysis, the top 4 models of the top 10 clusters
 are shown. In case seletopclusts is run after a sampling module, we can
-keep a few models from all the clusters to have more diversity in at the
-refinement stage.
+keep a few models from all the clusters to have more diversity at the
+refinement stage(s).
 """
-import math
-import os
+
 from pathlib import Path
 
+from haddock import log
 from haddock.core.typing import Any, FilePath
 from haddock.modules import BaseHaddockModule
+from haddock.modules.analysis.seletopclusts.seletopclusts import (
+    select_top_clusts_models,
+    write_selected_models,
+    )
 
 
 RECIPE_PATH = Path(__file__).resolve().parent
@@ -48,64 +52,28 @@ class HaddockModule(BaseHaddockModule):
             _msg = "top_cluster must be an integer."
             self.finish_with_error(_msg)
 
+        # Retrieve list of previous models
         models_to_select = self.previous_io.retrieve_models()
 
-        # how many models should we output?
-        self.output_models = []
-        target_rankings = list(set([p.clt_rank for p in models_to_select]))
-        if self.params["top_cluster"] >= len(target_rankings):
-            # select all clusters
-            target_rankins_str = ",".join(map(str, target_rankings))
-            self.log(f"Selecting all clusters: {target_rankins_str}")
-        else:
-            # select top_cluster clusters
-            target_rankings = list(range(1, self.params["top_cluster"] + 1))
-            target_rankins_str = ",".join(map(str, target_rankings))
-            self.log((f"Selecting top {self.params['top_cluster']} clusters: "
-                      f"{target_rankins_str}"))
+        # Make model selection
+        selected_models, _notes = select_top_clusts_models(
+            self.params["method"],
+            models_to_select,
+            self.params["top_cluster"],
+            self.params["top_models"],
+            )
+        # Log notes
+        for note in _notes:
+            log.info(note)
 
-        # select the models to export
-        models_to_export = []
-        for target_ranking in target_rankings:
-            if math.isnan(self.params["top_models"]):
-                for pdb in models_to_select:
-                    if pdb.clt_rank == target_ranking:
-                        models_to_export.append(pdb)
-            else:
-                for model_ranking in range(1, self.params["top_models"] + 1):
-                    for pdb in models_to_select:
-                        if (
-                                pdb.clt_rank == target_ranking
-                                and pdb.clt_model_rank == model_ranking
-                                ):
-                            self.log(
-                                f" {pdb.file_name} "
-                                f"> cluster_{target_ranking}_"
-                                f"model_{model_ranking}.pdb"
-                                )
-                            models_to_export.append(pdb)
-        
         # dump the models to disk and change their attributes
-        with open('seletopclusts.txt', 'w') as fh:
-            fh.write("rel_path\tori_name\tcluster_name\tmd5" + os.linesep)
-            for model in models_to_export:
-                name = (
-                    f"cluster_{model.clt_rank}_model"
-                    f"_{model.clt_model_rank}.pdb")
-                # writing name
-                fh.write(
-                    f"{model.rel_path}\t"
-                    f"{model.ori_name}\t"
-                    f"{name}\t"
-                    f"{model.md5}" + os.linesep)
-                # changing attributes
-                name_path = Path(name)
-                name_path.write_text(model.rel_path.read_text())
-                model.ori_name = model.file_name
-                model.file_name = name
-                model.full_name = name
-                model.rel_path = Path('..', Path(self.path).name, name)
-                model.path = str(Path(self.path).resolve())
+        renamed_models = write_selected_models(
+            "seletopclusts.txt",
+            selected_models,
+            self.path,
+            )
 
-        self.output_models = models_to_export
+        # Make these new models the output of this module
+        self.output_models = renamed_models
+        # Export outputs
         self.export_io_models()

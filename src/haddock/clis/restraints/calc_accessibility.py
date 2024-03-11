620ffe3326027282a02b0f0e592496e6bfae9564
VGPReys <v.g.p.reys@uu.nl>
2024-03-11 14:06:32 +0100

    Reviewing process
    - Lints
    - Pre-fixed names for file.actpass
    - Blank line before passive resiudes
    - Some types

--- src/haddock/clis/restraints/calc_accessibility.py
+++ src/haddock/clis/restraints/calc_accessibility.py
@@ -1,25 +1,33 @@
 """haddock3-restraints calc_accessibility subcommand.
 
-Given a pdb file, it will calculate the relative accessibility of the side chains
-and return a list of surface exposed residues.
+Given a pdb file, it will calculate the relative accessibility of
+the side chains and return a list of surface exposed residues.
 
 Nucleic acids bases are considered to be always accessible.
 
 Usage:
-    haddock3-restraints calc_accessibility <pdb_file> [-c <cutoff>] [--log_level <log_level>] [--export_to_actpass]
+    haddock3-restraints calc_accessibility
+        <input_pdb_file>            Path to a PDB file to analyse
+        [-c <cutoff>]               Relative accessibility cutoff
+        [--log_level <log_level>]   DEBUG, INFO, WARNING, ERROR, or CRITICAL
+        [--export_to_actpass]       Flag to export accessible resiudes
 """
 
-import argparse
-import sys
+
 import logging
 import os
 from freesasa import Structure
 from pathlib import Path
 
+from haddock.core.typing import Union
+
+
+# As this script is a subcommand of the `haddock3-restraints` cli,
+# it requires its own options and arguments that are managed here.
 def add_calc_accessibility_arguments(calc_accessibility_subcommand):
     """Add arguments to the calc_accessibility subcommand."""
     calc_accessibility_subcommand.add_argument(
-        "pdb_file",
+        "input_pdb_file",
         type=str,
         help="input PDB structure.",
         )
@@ -51,19 +59,31 @@ def add_calc_accessibility_arguments(calc_accessibility_subcommand):
 
     return calc_accessibility_subcommand
 
-default_bases = ["DA", "DC", "DG", "DT", "A", "C", "G", "U"]
-modified_bases = ["DJ"]
-standard_aa = ['ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO',
-               'SER', 'THR', 'TRP', 'TYR', 'VAL']
-modified_aa = ['ACE', 'ALY', 'ASH', 'CFE', 'CHX', 'CSP', 'CTN', 'CYC', 'CYF', 'CYM', 'DDZ', 'DUM', 'GLH', 'HLY', 'HYP',
-               'M3L', 'MLY', 'MLZ', 'MSE', 'NEP', 'PNS', 'PTR', 'SEP', 'TOP', 'TYP', 'TYS']  # , 'THP'='TOP'?
-valid_aa = standard_aa + modified_aa
-valid_bases = default_bases + modified_bases
-valid_ions = ["LI", "F", "NA", "MG", "AL", "CL", "K", "CA", "V", "CR", "MN", "FE", "NI", "CO", "CU", "ZN",
-              "BR", "KR", "SR", "MO", "AG", "CD", "I", "CS", "HO", "YB", "OS", "IR", "PT", "AU", "HG", "PB"]
+
+##########################################################
+# Define set of resiudes / bases handeled by this script #
+##########################################################
+DEFAULT_BASES = ["DA", "DC", "DG", "DT", "A", "C", "G", "U"]
+MODIFIED_BASES = ["DJ"]
+STANDARD_AA = [
+    'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE',
+    'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL',
+    ]
+MODIFIED_AA = [
+    'ACE', 'ALY', 'ASH', 'CFE', 'CHX', 'CSP', 'CTN', 'CYC', 'CYF', 'CYM',
+    'DDZ', 'DUM', 'GLH', 'HLY', 'HYP', 'M3L', 'MLY', 'MLZ', 'MSE', 'NEP',
+    'PNS', 'PTR', 'SEP', 'TOP', 'TYP', 'TYS',
+    ]  # , 'THP'='TOP'?
+VALID_AA = STANDARD_AA + MODIFIED_AA
+VALID_BASES = DEFAULT_BASES + MODIFIED_BASES
+VALID_IONS = [
+    "LI", "F", "NA", "MG", "AL", "CL", "K", "CA", "V", "CR", "MN", "FE", "NI",
+    "CO", "CU", "ZN", "BR", "KR", "SR", "MO", "AG", "CD", "I", "CS", "HO",
+    "YB", "OS", "IR", "PT", "AU", "HG", "PB",
+    ]
 
 # Taken from NACCESS
-rel_asa = {
+REL_ASA = {
     'total':
         {
             'ALA': 107.95,
@@ -196,29 +216,44 @@ rel_asa = {
     }
 
 
-def get_accessibility(pdb_f):
-    """Calls `FreeSASA <https://freesasa.github.io/>`_ using its Python API and returns per-residue accessibility
-    values."""
+def get_accessibility(pdb_f: Union[Path, str]) -> dict[str, list[int]]:
+    """Compute per-residue accessibility values.
+    
+    Calls `FreeSASA <https://freesasa.github.io/>`_ using its Python API
+    and returns per-residue accessibility values.
+
+    Parameters
+    ----------
+    pdb_f : Union[Path, str]
+        Path to the PDB file of interest.
+    
+    Return
+    ------
+    resid_access : dict[str, list[int]]
+        Dictionary containing a list of accessible residues for each chain(s).
+    """
     naccess_unsupported_aa = ['HEC', 'TIP', 'ACE', 'THP', 'HEB', 'CTN']
     logging.info("Calculate accessibility...")
     try:
-        from freesasa import Classifier, structureFromBioPDB, calc
+        from freesasa import Classifier, calc
     except ImportError as err:
-        print('[!] calc_accessibility requires the \'freesasa\' Python API', file=sys.stderr)
+        logging.error("calc_accessibility requires the 'freesasa' Python API")
         raise ImportError(err)
 
     asa_data, rsa_data, rel_main_chain, rel_side_chain = {}, {}, {}, {}
-    _rsa = rel_asa['total']
-    _rsa_bb = rel_asa['bb']
-    _rsa_sc = rel_asa['sc']
+    _rsa = REL_ASA['total']
+    _rsa_bb = REL_ASA['bb']
+    _rsa_sc = REL_ASA['sc']
 
-    # Workaround to get relative accessibility values for Nucleic Acids (will always be accessible)
-    for na in valid_bases:
+    # Workaround to get relative accessibility values for Nucleic Acids
+    #  -> will always be accessible !
+    for na in VALID_BASES:
         _rsa[na] = 1
         _rsa_bb[na] = 1
         _rsa_sc[na] = 1
 
-    config_path = '/'.join(os.path.realpath(__file__).split('/')[:-1]) + '/naccess.config'
+    _script_path = '/'.join(os.path.realpath(__file__).split('/')[:-1])
+    config_path = _script_path + '/naccess.config'
     classifier = Classifier(config_path)
 
     struct = Structure(pdb_f, classifier, options={})
@@ -239,51 +274,71 @@ def get_accessibility(pdb_f):
         # add asa to residue
         rsa_data[res_uid] = rsa_data.get(res_uid, 0) + asa
 
-        # If residue name is unknown, this is probably an ion, set a relative accessibility of -1
-        if resname not in _rsa and (resname not in valid_aa + valid_bases or resname[:2] in valid_ions
-                                    or resname in naccess_unsupported_aa):
+        # If residue name is unknown, this is probably an ion,
+        #  set a relative accessibility of -1
+        if resname not in _rsa and (
+                resname not in VALID_AA + VALID_BASES
+                or resname[:2] in VALID_IONS
+                or resname in naccess_unsupported_aa
+                ):
             logging.warning(f"UPDATED RSA for {resname}")
             _rsa[resname] = -1
             _rsa_bb[resname] = -1
             _rsa_sc[resname] = -1
 
         # 3 cases: Regular amino-acid, regular nucleic acid, ion
-        if (resname not in valid_bases and atname in ('C', 'N', 'O')) or \
-                (resname in valid_bases and atname in ('P', 'C1', 'C9')):
+        if (resname not in VALID_BASES and atname in ('C', 'N', 'O')) or \
+                (resname in VALID_BASES and atname in ('P', 'C1', 'C9')):
             rel_main_chain[res_uid] = rel_main_chain.get(res_uid, 0) + asa
-        elif resname not in valid_aa + valid_bases and resname[:2] in valid_ions:
+        elif all([
+                resname not in VALID_AA + VALID_BASES,
+                resname[:2] in VALID_IONS,
+                ]):
             rel_main_chain[res_uid] = rel_main_chain.get(res_uid, 0) + asa
             rel_side_chain[res_uid] = rel_side_chain.get(res_uid, 0) + asa
         else:
             rel_side_chain[res_uid] = rel_side_chain.get(res_uid, 0) + asa
-    # convert total asa ro relative asa
-    rsa_data.update((res_uid, asa / _rsa[res_uid[1]]) for res_uid, asa in rsa_data.items())
-    rel_main_chain.update((res_uid, asa / _rsa_bb[res_uid[1]] * 100) for res_uid, asa in rel_main_chain.items())
-    rel_side_chain.update((res_uid, asa / _rsa_sc[res_uid[1]] * 100) for res_uid, asa in rel_side_chain.items())
+    # convert total asa to relative asa
+    rsa_data.update(
+        (res_uid, asa / _rsa[res_uid[1]])
+        for res_uid, asa in rsa_data.items()
+        )
+    rel_main_chain.update(
+        (res_uid, asa / _rsa_bb[res_uid[1]] * 100)
+        for res_uid, asa in rel_main_chain.items()
+        )
+    rel_side_chain.update(
+        (res_uid, asa / _rsa_sc[res_uid[1]] * 100)
+        for res_uid, asa in rel_side_chain.items()
+        )
     # We format to fit the pipeline
-    resid_access = {}
+    resid_access: dict[str, list[int]] = {}
     for res_uid, access in rel_main_chain.items():
         chain = res_uid[0]
         # resname = res_uid[1]
         resnum = res_uid[2]
         if chain not in resid_access:
             resid_access[chain] = {}
-        resid_access[chain][resnum] = {'side_chain_rel': rel_side_chain.get(res_uid, 0), 'main_chain_rel': access}
+        resid_access[chain][resnum] = {
+            'side_chain_rel': rel_side_chain.get(res_uid, 0),
+            'main_chain_rel': access,
+            }
+    # Display accessible residues
     for chain in resid_access:
         logging.info(f"Chain: {chain} - {len(resid_access[chain])} residues")
     return resid_access
 
 
-def apply_cutoff(access_data, cutoff):
-    """Apply a cutoff to the sidechain relative accessibility and display on stdout"""
+def apply_cutoff(access_data: dict, cutoff: float) -> dict[str, list[int]]:
+    """Apply a cutoff to the sidechain relative accessibility."""
     logging.info(f'Applying cutoff to side_chain_rel - {cutoff}')
     # saving the results in a dictionary
-    result_dict = {}
+    result_dict: dict[str, list[int]] = {}
     for chain in access_data:
-        result_list = []
+        result_list: list[int] = []
         for res in access_data[chain]:
-            sidechain_relative_accessibility = access_data[chain][res]['side_chain_rel']
-            if sidechain_relative_accessibility >= cutoff * 100:
+            sc_rel_accessibility = access_data[chain][res]['side_chain_rel']
+            if sc_rel_accessibility >= cutoff * 100:
                 result_list.append(res)
         result_list = list(set(result_list))
         result_list.sort()
@@ -294,40 +349,56 @@ def apply_cutoff(access_data, cutoff):
     return result_dict
 
 
-def export_passive(result_dict):
-    """Export the exposed residues as passive to an actpass file (one per chain)."""
-    if result_dict != {}:
-        for chain in result_dict:
-            filename = Path(f"passive_{chain}.actpass")
-            logging.info(f"Writing exposed residues as passive to {filename}")
-            if filename.exists():
-                logging.warning(f"File {filename} already exists...")
-            else:
-                with open(filename, "w") as f:
-                    f.write(f"{' '.join(map(str, result_dict[chain]))}")
+def export_passive(
+        result_dict: dict[str, list[int]],
+        prefix: str = '',
+        ):
+    """Export the exposed residues as passive to an actpass file."""
+    for chain in result_dict:
+        filename = Path(f"{prefix}passive_{chain}.actpass")
+        logging.info(f"Writing exposed residues as passive in: {filename}")
+        if filename.exists():
+            logging.error(
+                f"File {filename} already exists, nothing performed!"
+                )
+        else:
+            with open(filename, "w") as f:
+                f.write(os.linesep)  # Add empty line as no active residues
+                f.write(f"{' '.join(map(str, result_dict[chain]))}")
 
 
-def calc_accessibility(pdb_file, cutoff=0.4, log_level="INFO", export_to_actpass=False):
+def calc_accessibility(
+        input_pdb_file,
+        cutoff: float = 0.4,
+        log_level: str = "INFO",
+        export_to_actpass: bool = False,
+        ):
     """Calculate the accessibility of the side chains and apply a cutoff.
     
     Parameters
     ----------
-    pdb_file : str
+    input_pdb_file : str
         Path to the PDB file.
     cutoff : float
-        Relative cutoff for sidechain accessibility.
+        Relative cutoff for sidechain accessibility, default = 0.4
     log_level : str
         Logging level.
     export_to_actpass : bool
         Export the exposed residues as passive to an actpass file.
     """
-    logging.basicConfig(level=log_level,
-                        format='%(asctime)s L%(lineno)d %(levelname)s - %(message)s',
-                        datefmt='%d/%m/%Y %H:%M:%S')
-
-    access_dic = get_accessibility(pdb_file)
+    logging.basicConfig(
+        level=log_level,
+        format='%(asctime)s L%(lineno)d %(levelname)s - %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S',
+        )
+    # Compute per-residues accessibilities
+    access_dic = get_accessibility(input_pdb_file)
+    # Filter residues based on accessibility cutoff
     result_dict = apply_cutoff(access_dic, cutoff)
 
     # export residues as passive to an actpass file
     if export_to_actpass:
-        export_passive(result_dict)
+        export_passive(
+            result_dict,
+            prefix=f'{Path(input_pdb_file).stem}_',
+            )

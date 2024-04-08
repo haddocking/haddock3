9ea265e9fa0bcf1b33b044a5f0c86fa41f54cd9b
mgiulini <mrcgiulini@gmail.com>
2024-04-08 16:40:28 +0200

    added test for xyzwriter

--- tests/test_module_rmsdmatrix.py
+++ tests/test_module_rmsdmatrix.py
@@ -1,6 +1,7 @@
 """Test the rmsdmatrix module."""
 import os
 from pathlib import Path
+import tempfile
 
 import numpy as np
 import pytest
@@ -9,9 +10,9 @@ from haddock.libs.libontology import PDBFile
 from haddock.modules.analysis.rmsdmatrix import DEFAULT_CONFIG as rmsd_pars
 from haddock.modules.analysis.rmsdmatrix import HaddockModule
 from haddock.modules.analysis.rmsdmatrix.rmsd import (
-    RMSDJob,
     get_pair,
     rmsd_dispatcher,
+    XYZWriter,
     )
 
 from . import golden_data
@@ -106,3 +107,47 @@ def test_overall_rmsd(input_protdna_models):
     os.unlink(Path("rmsd.matrix"))
     os.unlink(Path("rmsd_matrix.json"))
     os.unlink(Path("io.json"))
+
+
+def test_xyzwriter(input_protdna_models):
+    "test XYZWriter"
+    with tempfile.TemporaryDirectory() as tmpdir:
+        common_keys = [
+            ('A', 10, 'N'),
+            ('A', 10, 'CA'),
+            ('A', 32, 'N'),
+            ('B', 38, 'C6'),
+        ]
+        exp_output = Path(tmpdir, "test.xyz")
+        xyzwriter_obj = XYZWriter(
+            model_list=input_protdna_models,
+            output_name=exp_output,
+            core=1,
+            n_atoms=4,
+            common_keys=common_keys,
+            filter_resdic=None,
+            allatoms=False,
+            )
+        xyzwriter_obj.run()
+        
+        assert exp_output.exists()
+        # check the content
+        with open(exp_output) as f:
+            content = f.read()
+        exp_content_list = [
+            "4",
+            "",
+            "A10N 12.163 -4.828 -1.492",
+            "A10CA 11.392 -5.83 -0.759",
+            "A32N 0.127 -0.923 -0.471",
+            "B38C6 -6.564 -18.595 -5.571",
+            "4",
+            "",
+            "A10N -11.179 7.766 -1.6",
+            "A10CA -9.966 8.514 -1.292",
+            "A32N -1.291 -0.108 -2.675",
+            "B38C6 14.422 15.302 -5.743",
+        ]
+
+        exp_content = os.linesep.join(exp_content_list) + os.linesep
+        assert content == exp_content

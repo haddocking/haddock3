b3cb0d88009f3483f6d7abe98bca4e88b2b832a8
amjjbonvin <alexandre.bonvin@gmail.com>
2024-05-01 20:28:50 +0200

    Added new rigid-body redocking module For use after a refinement module

---
+++ src/haddock/modules/sampling/rigidredock/__init__.py
@@ -0,0 +1,165 @@
+"""
+Rigid-body re-docking refinement module
+=======================================
+
+This module does a **randomization of orientations and rigid-body
+minimization.** It corresponds to the classical ``it0`` step in the HADDOCK2.x
+series, but meant to be used after a refinement module to redock refined models.
+
+In this module, the interacting partners are treated as rigid bodies, meaning
+that all geometrical parameters such as bond lengths, bond angles, and dihedral
+angles are frozen. The partners are first separated in space and randomly
+rotated around their respective centres of mass. Afterwards, the molecules are
+brought together by rigid-body energy minimisation with rotations and
+translation as the only degrees of freedom.
+
+The driving force for this energy minimisation is the energy function, which
+consists of the intermolecular van der Waals and electrostatic energy terms and
+the restraints defined to guide the docking. The restraints are distance-based
+and can consist of unambiguous or ambiguous interactions restraints (AIRS). In
+*ab-initio* docking mode those restraints can be automatically defined in
+various ways; e.g. between center of masses (CM restraints) or between randomly
+selected patches on the surface (random AIRs).
+
+The definition of those restraints is particularly important as they effectively
+guide the minimisation process. For example, with a stringent set of AIRs or
+unambiguous distance restraints, the solutions of the minimisation will converge
+much better and the sampling can be limited. In *ab-initio* mode, however, very
+diverse solutions will be obtained and the sampling should be increased to make
+sure to sample enough the possible interaction space.
+"""
+from pathlib import Path
+
+from haddock.core.typing import FilePath
+from haddock.gear.haddockmodel import HaddockModel
+from haddock.libs.libcns import prepare_cns_input, prepare_expected_pdb
+from haddock.libs.libontology import PDBFile
+from haddock.libs.libsubprocess import CNSJob
+from haddock.modules import get_engine
+from haddock.modules.base_cns_module import BaseCNSModule
+
+
+RECIPE_PATH = Path(__file__).resolve().parent
+DEFAULT_CONFIG = Path(RECIPE_PATH, "defaults.yaml")
+
+
+class HaddockModule(BaseCNSModule):
+    """HADDOCK3 module for rigid body sampling."""
+
+    name = RECIPE_PATH.name
+
+    def __init__(self,
+                 order: int,
+                 path: Path,
+                 initial_params: FilePath = DEFAULT_CONFIG) -> None:
+        cns_script = Path(RECIPE_PATH, "cns", "rigidredock.cns")
+        super().__init__(order, path, initial_params, cns_script=cns_script)
+
+    @classmethod
+    def confirm_installation(cls) -> None:
+        """Confirm module is installed."""
+        return
+
+    def _run(self) -> None:
+        """Execute module."""
+        # Pool of jobs to be executed by the CNS engine
+        jobs: list[CNSJob] = []
+
+        # Get the models generated in previous step
+        try:
+            models_to_refine = self.previous_io.retrieve_models()
+        except Exception as e:
+            self.finish_with_error(e)
+
+        self.output_models: list[PDBFile] = []
+        idx = 1
+        sampling_factor = self.params["sampling_factor"]
+        if sampling_factor > 1:
+            self.log(f"sampling_factor={sampling_factor}")
+        if sampling_factor == 0:
+            self.log("[Warning] sampling_factor cannot be 0, setting it to 1")
+            sampling_factor = 1
+        if sampling_factor > 100:
+            self.log("[Warning] sampling_factor is larger than 100")
+
+        max_nmodels = self.params["max_nmodels"]
+        nmodels = len(models_to_refine) * sampling_factor
+        if nmodels > max_nmodels:
+            self.finish_with_error(
+                f"Too many models ({nmodels}) to refine, max_nmodels ="
+                f" {max_nmodels}. Please reduce the number of models or"
+                " decrease the sampling_factor."
+                )
+
+        # checking the ambig_fname:
+        try:
+            prev_ambig_fnames = [mod.restr_fname for mod in models_to_refine]
+        except Exception as e:  # noqa:F841
+            # cannot extract restr_fname info from tuples
+            prev_ambig_fnames = [None for model in models_to_refine]
+
+        ambig_fnames = self.get_ambig_fnames(prev_ambig_fnames)
+
+        model_idx = 0
+        idx = 1
+        for model in models_to_refine:
+            # assign ambig_fname
+            if ambig_fnames:
+                ambig_fname = ambig_fnames[model_idx]
+            else:
+                ambig_fname = self.params["ambig_fname"]
+            model_idx += 1
+
+            for _ in range(self.params['sampling_factor']):
+                # prepare cns input
+                inp_file = prepare_cns_input(
+                    idx,
+                    model,
+                    self.path,
+                    self.recipe_str,
+                    self.params,
+                    "rigidredock",
+                    ambig_fname=ambig_fname,
+                    native_segid=True,
+                    )
+
+                out_file = f"rigidredock_{idx}.out"
+
+                # create the expected PDBobject
+                expected_pdb = prepare_expected_pdb(
+                    model, idx, ".", "rigidredock"
+                    )
+                expected_pdb.restr_fname = ambig_fname
+                try:
+                    expected_pdb.ori_name = model.file_name
+                except AttributeError:
+                    expected_pdb.ori_name = None
+                self.output_models.append(expected_pdb)
+
+                job = CNSJob(inp_file, out_file, envvars=self.envvars)
+
+                jobs.append(job)
+
+                idx += 1
+
+        # Run CNS Jobs
+        self.log(f"Running CNS Jobs n={len(jobs)}")
+        Engine = get_engine(self.params['mode'], self.params)
+        engine = Engine(jobs)
+        engine.run()
+        self.log("CNS jobs have finished")
+
+        # Get the weights from the defaults
+        _weight_keys = ("w_vdw", "w_elec", "w_desolv", "w_air", "w_bsa")
+        weights = {e: self.params[e] for e in _weight_keys}
+
+        for pdb in self.output_models:
+            if pdb.is_present():
+                haddock_model = HaddockModel(pdb.file_name)
+                pdb.unw_energies = haddock_model.energies
+
+                haddock_score = haddock_model.calc_haddock_score(**weights)
+                pdb.score = haddock_score
+
+        # Save module information
+        self.export_io_models(faulty_tolerance=self.params["tolerance"])

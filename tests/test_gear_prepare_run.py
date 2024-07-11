afd065a8c811d350f9804a7d51fff09d5470947d
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-07-11 15:54:38 +0200

    add `test_validate_parameters_are_not_incompatible`

--- tests/test_gear_prepare_run.py
+++ tests/test_gear_prepare_run.py
@@ -1,4 +1,5 @@
 """Test prepare run module."""
+
 import shutil
 from math import isnan
 from pathlib import Path
@@ -19,7 +20,8 @@ from haddock.gear.prepare_run import (
     validate_param_type,
     validate_parameters_are_not_misspelled,
     validate_value,
-    )
+    validate_parameters_are_not_incompatible,
+)
 from haddock.gear.yaml2cfg import read_from_yaml_config
 from haddock.modules import modules_names
 from haddock.modules.topology.topoaa import DEFAULT_CONFIG
@@ -37,11 +39,11 @@ DEFAULT_DICT = read_from_yaml_config(DEFAULT_CONFIG)
             {
                 "autohis": None,
                 "mol1": {"nhisd", "hisd_1", "hisd_2", "nhise", "hise_1"},
-                },
+            },
             {"hisd_1", "hisd_2", "hise_1"},
-            )
-        ]
-    )
+        )
+    ],
+)
 def test_get_expandable_parameters_topoaa(inp, expected):
     """Test get blocks."""
     result = get_expandable_parameters(inp, DEFAULT_DICT, "topoaa", 20)
@@ -53,7 +55,7 @@ def test_populate_topoaa_molecules():
     topoaa = {
         "molecules": ["file1.pdb", "file2.pdb"],
         "mol1": {"cyclicpept": True},
-        }
+    }
     populate_topology_molecule_params(topoaa)
     assert "mol2" in topoaa
     assert topoaa["mol2"]["cyclicpept"] is False
@@ -73,7 +75,7 @@ def test_populate_topoaa_molecules_2():
     topoaa = {
         "molecules": ["file1.pdb", "file2.pdb"],
         "mol2": {"cyclicpept": True},
-        }
+    }
     populate_topology_molecule_params(topoaa)
     assert "mol1" in topoaa
 
@@ -96,7 +98,7 @@ def test_populate_topoaa_molecules_3():
     topoaa = {
         "molecules": ["file1.pdb", "file2.pdb", "file3.pdb"],
         "mol2": {"cyclicpept": True},
-        }
+    }
     populate_topology_molecule_params(topoaa)
     assert "mol1" in topoaa
 
@@ -106,7 +108,7 @@ def test_populate_topoaa_molecules_4():
     topoaa = {
         "molecules": ["file1.pdb", "file2.pdb", "file3.pdb", "file4.pdb"],
         "mol3": {"cyclicpept": True},
-        }
+    }
     populate_topology_molecule_params(topoaa)
     assert "mol1" in topoaa
 
@@ -117,7 +119,7 @@ def test_populate_mol_params():
         "topoaa.1": {"molecules": ["file1.pdb", "file2.pdb", "file3.pdb"]},
         "flexref.1": {"mol_fix_origin_2": True},
         "caprieval.1": {},
-        }
+    }
 
     populate_mol_parameters(params)
     assert "mol_fix_origin_1" in params["flexref.1"]
@@ -140,8 +142,10 @@ def test_check_if_path_exists():
 
     with pytest.raises(ValueError) as err:
         check_if_path_exists("file-01.txt")
-        end = ("the following \'file_01.txt\' is the closest match to the "
-               "supplied \'file-01.txt\', did you mean to open this?")
+        end = (
+            "the following 'file_01.txt' is the closest match to the "
+            "supplied 'file-01.txt', did you mean to open this?"
+        )
         assert str(err).endswith(end)
 
     Path("file_01.txt").unlink()
@@ -152,12 +156,14 @@ def test_check_if_path_exists():
     "user_input,expected",
     [
         ("long-fromat", [("long-fromat", "long-format")]),
-        (["loong-format", "verboese"],
-            [("loong-format", "long-format"), ("verboese", "verbose")]),
+        (
+            ["loong-format", "verboese"],
+            [("loong-format", "long-format"), ("verboese", "verbose")],
+        ),
         ("middle-format", [("middle-format", "long-format")]),
         ("out", [("out", "output-dir")]),
-        ]
-    )
+    ],
+)
 def test_fuzzy_match(user_input, expected):
     possibilities = ["long-format", "short-format", "verbose", "output-dir"]
     assert fuzzy_match(user_input, possibilities) == expected
@@ -168,8 +174,8 @@ def test_fuzzy_match(user_input, expected):
     [
         ("mol1.pdb", 1),
         (["mol1.pdb", "mol2.pdb"], 2),
-        ],
-    )
+    ],
+)
 def test_copy_mols_to_topo_dir(molecules, expected):
     d = {}
     copy_molecules_to_topology(molecules, d)
@@ -181,15 +187,15 @@ def test_copy_mols_to_topo_dir(molecules, expected):
 def test_validate_step_names_are_not_misspelled():
     params = {
         "par1": None,
-        }
+    }
 
     for i, module in enumerate(modules_names):
         # the key and value for this function do not matter
         if i % 2 == 0:
-            params[module] = {None: None}
+            params[module] = {None: None}  # type: ignore
         else:
             # add index
-            params[module + ".1"] = {None: None}
+            params[module + ".1"] = {None: None}  # type: ignore
 
     validate_module_names_are_not_misspelled(params)
 
@@ -197,9 +203,9 @@ def test_validate_step_names_are_not_misspelled():
 def test_validate_step_names_are_not_misspelled_error():
     params = {
         "par1": None,
-        }
+    }
 
-    params["r1g1dbod1"] = {None: None}
+    params["r1g1dbod1"] = {None: None}  # type: ignore
 
     with pytest.raises(ValueError):
         validate_module_names_are_not_misspelled(params)
@@ -221,17 +227,17 @@ def test_validate_params_error():
 def test_update_step_folders_from_restart():
     output_tmp = Path(data_folder, "1_dummystep")
     shutil.copytree(steptmp, output_tmp)
-    prev_names = ['0_dummystep']
-    next_names = ['1_dummystep']
+    prev_names = ["0_dummystep"]
+    next_names = ["1_dummystep"]
     update_step_contents_to_step_names(prev_names, next_names, data_folder)
 
     file1 = Path(output_tmp, "file1.in").read_text()
-    assert '0_dummystep' not in file1
-    assert '1_dummystep' in file1
+    assert "0_dummystep" not in file1
+    assert "1_dummystep" in file1
 
     file2 = Path(output_tmp, "folder", "file2.in").read_text()
-    assert '0_dummystep' not in file2
-    assert '1_dummystep' in file2
+    assert "0_dummystep" not in file2
+    assert "1_dummystep" in file2
 
     shutil.rmtree(output_tmp)
 
@@ -239,14 +245,14 @@ def test_update_step_folders_from_restart():
 @pytest.mark.parametrize(
     "defaultparam,inputvalue",
     [
-        ({'type': 'integer'}, 1),
-        ({'type': 'float'}, 0.99),
-        ({'type': 'list'}, [1, 2]),
-        ({'type': 'boolean'}, False),
-        ({'type': 'boolean'}, True),
-        ({'type': 'string'}, 'Hello world'),
-        ]
-    )
+        ({"type": "integer"}, 1),
+        ({"type": "float"}, 0.99),
+        ({"type": "list"}, [1, 2]),
+        ({"type": "boolean"}, False),
+        ({"type": "boolean"}, True),
+        ({"type": "string"}, "Hello world"),
+    ],
+)
 def test_accepted_value_type(defaultparam, inputvalue):
     """Test if the provided value type is correct.
 
@@ -258,33 +264,33 @@ def test_accepted_value_type(defaultparam, inputvalue):
 @pytest.mark.parametrize(
     "defaultparam,inputvalue",
     [
-        ({'type': 'integer', 'default': 0}, False),
-        ({'type': 'float', 'default': 1.1}, 'Hello world'),
-        ({'type': 'list', 'default': [1, 2]}, {1: 1, 2: 2}),
-        ({'type': 'boolean', 'default': True}, 1),
-        ({'type': 'boolean', 'default': False}, 0),
-        ({'type': 'str', 'default': 'test'}, 2.1),
-        ]
-    )
+        ({"type": "integer", "default": 0}, False),
+        ({"type": "float", "default": 1.1}, "Hello world"),
+        ({"type": "list", "default": [1, 2]}, {1: 1, 2: 2}),
+        ({"type": "boolean", "default": True}, 1),
+        ({"type": "boolean", "default": False}, 0),
+        ({"type": "str", "default": "test"}, 2.1),
+    ],
+)
 def test_value_type_error(defaultparam, inputvalue):
     """Test if the provided value type is wrong.
 
     Should return a formated string describing the error.
     """
     assert validate_param_type(defaultparam, inputvalue) is not None
-    
+
 
 @pytest.mark.parametrize(
     "defaultparam,inputvalue",
     [
-        ({'min': 0, 'max': 1}, 0.5),
-        ({'min': 1, 'max': 1000}, 1),
-        ({'min': 0, 'max': 1000}, 1000),
-        ({'min': 0, 'max': 1000}, -0),
-        ({'choices': ['ok', 'fine']}, 'fine'),
-        ({'choices': ['super']}, 'super'),
-        ]
-    )
+        ({"min": 0, "max": 1}, 0.5),
+        ({"min": 1, "max": 1000}, 1),
+        ({"min": 0, "max": 1000}, 1000),
+        ({"min": 0, "max": 1000}, -0),
+        ({"choices": ["ok", "fine"]}, "fine"),
+        ({"choices": ["super"]}, "super"),
+    ],
+)
 def test_accepted_value_range(defaultparam, inputvalue):
     """Test if the provided value range/choices is correct.
 
@@ -296,13 +302,13 @@ def test_accepted_value_range(defaultparam, inputvalue):
 @pytest.mark.parametrize(
     "defaultparam,inputvalue",
     [
-        ({'min': 0, 'max': 1, 'default': 0}, 2),
-        ({'min': 1.1, 'max': 1.3, 'default': 1.2}, 1.4),
-        ({'min': 0.1, 'max': 1.0, 'default': 0.5}, -0.1),
-        ({'choices': ['not', 'good'], 'default': 'good'}, ['not', 'good']),
-        ({'choices': ['super'], 'default': 'super'}, 'wrong'),
-        ]
-    )
+        ({"min": 0, "max": 1, "default": 0}, 2),
+        ({"min": 1.1, "max": 1.3, "default": 1.2}, 1.4),
+        ({"min": 0.1, "max": 1.0, "default": 0.5}, -0.1),
+        ({"choices": ["not", "good"], "default": "good"}, ["not", "good"]),
+        ({"choices": ["super"], "default": "super"}, "wrong"),
+    ],
+)
 def test_value_range_error(defaultparam, inputvalue):
     """Test if the provided value range/choices is erronnated.
 
@@ -314,19 +320,17 @@ def test_value_range_error(defaultparam, inputvalue):
 @pytest.mark.parametrize(
     "defaultparams,key,value",
     [
-        ({'key': {'min': 0, 'max': 1, 'type': 'integer'}}, 'key', 1),
-        ({'key': {'min': 0.5, 'max': 2.2, 'type': 'float'}}, 'key', 1.1),
-        ({'key': {'min': 0, 'max': 1, 'type': 'integer'}}, 'key', 0),
-        ({'key': {'choices': ['ok', 'fine'], 'type': 'string'}},
-         'key', 'ok'),
-        ({'key': {'choices': ['ok', 'fine'], 'type': 'string'}},
-         'key', 'fine'),
-        ({'key': {'type': 'boolean'}}, 'key', True),
-        ({'key': {'type': 'list'}}, 'key', ['mol1', 'mol2']),
-        ({'mol': {'group': 'molecules', 'type': 'list'}}, 'mol', ['pdb']),
-        ({'molecule': {'type': 'list'}}, 'molecules', ['mol1', 'pdb']),
-        ]
-    )
+        ({"key": {"min": 0, "max": 1, "type": "integer"}}, "key", 1),
+        ({"key": {"min": 0.5, "max": 2.2, "type": "float"}}, "key", 1.1),
+        ({"key": {"min": 0, "max": 1, "type": "integer"}}, "key", 0),
+        ({"key": {"choices": ["ok", "fine"], "type": "string"}}, "key", "ok"),
+        ({"key": {"choices": ["ok", "fine"], "type": "string"}}, "key", "fine"),
+        ({"key": {"type": "boolean"}}, "key", True),
+        ({"key": {"type": "list"}}, "key", ["mol1", "mol2"]),
+        ({"mol": {"group": "molecules", "type": "list"}}, "mol", ["pdb"]),
+        ({"molecule": {"type": "list"}}, "molecules", ["mol1", "pdb"]),
+    ],
+)
 def test_accepted_param_value(defaultparams, key, value):
     """Test if the provided parameter = value is correct.
 
@@ -338,31 +342,44 @@ def test_accepted_param_value(defaultparams, key, value):
 @pytest.mark.parametrize(
     "defaultparams,key,value",
     [
-        ({'key': {'min': 0, 'max': 1, 'type': 'integer',
-                  'default': 0}}, 'key', 1.1),
-        ({'key': {'min': 0.5, 'max': 2.2,
-                  'type': 'float', 'default': 1.1}}, 'key', 0),
-        ({'key': {'choices': ['not', 'good'],
-                  'type': 'string',
-                  'default': 'good'}},
-         'key', 'ok'),
-        ({'key': {'choices': ['False', 'True'],
-                  'type': 'string',
-                  'default': 'True'}},
-         'key', True),
-        ({'key': {'choices': ['False', 'True'], 'type': 'string',
-                  'default': 'False'}},
-         'key', False),
-        ({'key': {'type': 'boolean', 'default': False}}, 'key', 0),
-        ({'key': {'type': 'boolean', 'default': True}}, 'key', 1),
-        ({'key': {'type': 'boolean', 'default': False}}, 'key', []),
-        ({'key': {'type': 'boolean', 'default': False}}, 'key', ''),
-        ({'key': {'type': 'list', 'default': ['m1', 'm2']}}, 'key', 1),
-        ({'key': {'type': 'list', 'default': ['m1', 'm2']}}, 'key', 'list'),
-        ({'key': {'type': 'list', 'default': ['m1', 'm2']}},
-         'key', {'hello': 'world'}),
-        ]
-    )
+        ({"key": {"min": 0, "max": 1, "type": "integer", "default": 0}}, "key", 1.1),
+        ({"key": {"min": 0.5, "max": 2.2, "type": "float", "default": 1.1}}, "key", 0),
+        (
+            {"key": {"choices": ["not", "good"], "type": "string", "default": "good"}},
+            "key",
+            "ok",
+        ),
+        (
+            {
+                "key": {
+                    "choices": ["False", "True"],
+                    "type": "string",
+                    "default": "True",
+                }
+            },
+            "key",
+            True,
+        ),
+        (
+            {
+                "key": {
+                    "choices": ["False", "True"],
+                    "type": "string",
+                    "default": "False",
+                }
+            },
+            "key",
+            False,
+        ),
+        ({"key": {"type": "boolean", "default": False}}, "key", 0),
+        ({"key": {"type": "boolean", "default": True}}, "key", 1),
+        ({"key": {"type": "boolean", "default": False}}, "key", []),
+        ({"key": {"type": "boolean", "default": False}}, "key", ""),
+        ({"key": {"type": "list", "default": ["m1", "m2"]}}, "key", 1),
+        ({"key": {"type": "list", "default": ["m1", "m2"]}}, "key", "list"),
+        ({"key": {"type": "list", "default": ["m1", "m2"]}}, "key", {"hello": "world"}),
+    ],
+)
 def test_param_value_error(defaultparams, key, value):
     """Test if the provided parameter = value is not correct.
 
@@ -370,3 +387,25 @@ def test_param_value_error(defaultparams, key, value):
     """
     with pytest.raises(ConfigurationError):
         validate_value(defaultparams, key, value)
+
+
+def test_validate_parameters_are_not_incompatible(mocker):
+    mocker.patch(
+        "haddock.gear.prepare_run.incompatible_params",
+        {"limiting_parameter": {"incompatible_parameter": "incompatible_value"}},
+    )
+
+    params = {
+        "limiting_parameter": "",
+        "incompatible_parameter": "incompatible_value",
+    }
+
+    with pytest.raises(ValueError):
+        validate_parameters_are_not_incompatible(params)
+
+    params = {
+        "limiting_parameter": "limiting_value",
+        "ok_parameter": "ok_value",
+    }
+
+    assert validate_parameters_are_not_incompatible(params) is None

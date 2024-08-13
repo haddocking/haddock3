9a3ff827b5622babbbcf0164d8fe4e8349756b3f
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-08-13 13:42:18 +0200

    update `gen_beads.sh`

--- examples/docking-protein-surface/gen_beads.sh
+++ examples/docking-protein-surface/gen_beads.sh
@@ -1,11 +1,35 @@
 # Generates only one plan based on one selection of residues
-haddock3-restraints z_surface_restraints --pdb data/protein.pdb --residues 19,83,145,119,167 --output data/one_z_surface_selection
+./haddock-restraints z \
+  --residues 19,83,145,119,167 \
+  data/protein.pdb \
+  data/one_z_surface_selection_new.pdb \
+  20 \
+  6 > data/one_z_surface_selection_beads_new.tbl
 
 # Generates two plans based on the two residues selections
-haddock3-restraints z_surface_restraints --pdb data/protein.pdb --residues 19,83,145,119,167 98,101,126,129 --output data/protein_z_restraints
+./haddock-restraints z \
+  --residues 19,83,145,119,167 \
+  --residues 98,101,126,129 \
+  data/protein.pdb \
+  data/protein_z_restraints_beads_new.pdb \
+  20 \
+  6  > data/protein_z_restraints_new.tbl
 
 # Generates two plans based on the two residues selections and reduce both area of the surface and space between beads
-haddock3-restraints z_surface_restraints --pdb data/protein.pdb --residues 19,83,145,119,167 98,101,126,129 --output data/protein_z_restraints_smallspacing --spacing 4 --x-size 20 --y-size 20
+./haddock-restraints z \
+  --residues 19,83,145,119,167 \
+  --residues 98,101,126,129 \
+  data/protein.pdb \
+  data/protein_z_restraints_smallspacing_beads_new.pdb \
+  20 \
+  4  > data/protein_z_restraints_smallspacing_new.tbl
 
 # Generates three plans based on the three residues selections
-haddock3-restraints z_surface_restraints --pdb data/protein.pdb --residues 19,83,145,119,167 98,101,126,129 23,62,87,111,116,153,163 --output data/three_z_restraints
+./haddock-restraints z \
+  --residues 19,83,145,119,167 \
+  --residues 98,101,126,129 \
+  --residues 23,62,87,111,116,153,163 \
+  data/protein.pdb \
+  data/three_z_restraints_beads_new.pdb \
+  20 \
+  6  > data/three_z_restraints_new.tbl

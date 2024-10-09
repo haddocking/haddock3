9606d90f4e556fe3e351e4b22a785e02687ab32b
VGPReys <v.g.p.reys@uu.nl>
2024-10-09 10:38:21 +0200

    managing cluster plots

--- src/haddock/libs/libfunc.py
+++ src/haddock/libs/libfunc.py
@@ -3,6 +3,9 @@ Tools for Functional-Programming in Python.
 
 From: https://github.com/joaomcteixeira/libfuncpy
 """
+import time
+
+from functools import wraps
 from operator import is_not
 from typing import Any, Callable, Literal
 
@@ -83,3 +86,15 @@ def nan(*ignore: Any, **everything: Any) -> float:
 def not_none(value: Any) -> bool:
     """Give True if value is not None, or False otherwise."""
     return is_not(value, None)
+
+
+def exec_time(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper

b38277d51592c7aab6cadb4ec79a56aac2a17326
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-10-21 17:32:15 +0200

    rework test_cli_analyse

--- tests/test_cli_analyse.py
+++ tests/test_cli_analyse.py
@@ -39,11 +39,15 @@ def example_capri_ss():
 
 def test_update_capri_dict(default_capri):
     """Test update_capri_dict."""
-    example_f = tempfile.NamedTemporaryFile(delete=False).name
+    example_f = tempfile.NamedTemporaryFile(
+        delete=False
+    ).name  # this is the tempfile.name
     ext_params = {"reference_fname": example_f}
     wrong_ext_params = {"wrong_parameter_name": 32}
     obs_capri_dict = update_capri_dict(default_capri, ext_params)
-    assert obs_capri_dict["reference_fname"].name == example_f
+    assert obs_capri_dict["reference_fname"].name == str(
+        Path(example_f).name
+    )  # this is the pathlib.name
     # when an invalid parameter is provided, it should exit
     with pytest.raises(SystemExit):
         update_capri_dict(default_capri, wrong_ext_params)
@@ -59,42 +63,35 @@ def test_get_cluster_ranking(example_capri_clt):
 def test_main(example_capri_ss, example_capri_clt):
     """Test cli_analyse main."""
     # build fake run_dir
-    run_dir = "example_dir"
-    if os.path.isdir(run_dir):
-        shutil.rmtree(run_dir)
-    step_name = "2_caprieval"
-    step_dir = Path(run_dir, step_name)
-    os.mkdir(run_dir)
-    os.mkdir(step_dir)
-    shutil.copy(example_capri_ss, Path(step_dir, "capri_ss.tsv"))
-    shutil.copy(example_capri_clt, Path(step_dir, "capri_clt.tsv"))
+    with tempfile.TemporaryDirectory() as tempdir:
+        os.chdir(tempdir)
+        step_dir = Path(tempdir, "2_caprieval")
+        step_dir.mkdir()
+        shutil.copy(example_capri_ss, Path(step_dir, "capri_ss.tsv"))
+        shutil.copy(example_capri_clt, Path(step_dir, "capri_clt.tsv"))
 
-    # run haddock3-analyse
-    main(
-        run_dir,
-        [2],
-        5,
-        format=None,
-        scale=None,
-        is_cleaned=False,
-        inter=False,
-    )
+        # run haddock3-analyse
+        main(
+            tempdir,
+            [2],
+            5,
+            format=None,
+            scale=None,
+            is_cleaned=False,
+            inter=False,
+        )
 
-    # check analysis directory exists
-    ana_dir = Path(run_dir, "analysis/")
-    assert os.path.isdir(ana_dir) is True
+        # check analysis directory exists
+        assert Path(tempdir, "analysis/").exists()
 
-    # check whether there are some html files
-    ana_subdir = Path(ana_dir, f"{step_name}_analysis")
-    html_files = [el for el in os.listdir(ana_subdir) if el.endswith(".html")]
-    assert len(html_files) > 0
-
-    shutil.rmtree(run_dir)
+        # check whether there are some html files
+        ana_subdir = Path(tempdir, "analysis", "2_caprieval_analysis")
+        html_files = [el for el in os.listdir(ana_subdir) if el.endswith(".html")]
+        assert len(html_files) > 0
 
 
 def test_zip_top_ranked(example_capri_ss):
     """Test cli_analyse zip_top_ranked function."""
-    cwd = os.getcwd()
     with tempfile.TemporaryDirectory() as tmpdir:
         os.chdir(tmpdir)
         # build fake run_dir
@@ -112,39 +109,33 @@ def test_zip_top_ranked(example_capri_ss):
         exp_cl_ranking = {1: 2}
         zip_top_ranked(example_capri_ss, exp_cl_ranking, "summary.tgz")
         assert os.path.isfile("summary.tgz") is True
-    os.chdir(cwd)
 
 
-def test_main_offline(example_capri_ss, example_capri_clt, tmp_path):
+def test_main_offline(example_capri_ss, example_capri_clt):
     """Test cli_analyse main in offline mode."""
-    # build fake run_dir
-    run_dir = tmp_path / "example_dir"
-    if os.path.isdir(run_dir):
-        shutil.rmtree(run_dir)
-    step_name = "2_caprieval"
-    step_dir = run_dir / step_name
-    os.mkdir(run_dir)
-    os.mkdir(step_dir)
-    shutil.copy(example_capri_ss, step_dir / "capri_ss.tsv")
-    shutil.copy(example_capri_clt, step_dir / "capri_clt.tsv")
 
-    # run haddock3-analyse
-    main(
-        run_dir,
-        [2],
-        5,
-        format=None,
-        scale=None,
-        is_cleaned=False,
-        inter=False,
-        offline=True,
-    )
+    with tempfile.TemporaryDirectory() as tempdir:
+        os.chdir(tempdir)
+        step_dir = Path(tempdir, "2_caprieval")
+        step_dir.mkdir()
+        shutil.copy(example_capri_ss, Path(step_dir, "capri_ss.tsv"))
+        shutil.copy(example_capri_clt, Path(step_dir, "capri_clt.tsv"))
 
-    # check analysis directory exists
-    ana_dir = run_dir / "analysis/"
-    assert ana_dir.is_dir()
+        main(
+            tempdir,
+            [2],
+            5,
+            format=None,
+            scale=None,
+            is_cleaned=False,
+            inter=False,
+            offline=True,
+        )
+        # check analysis directory exists
+        ana_dir = Path(tempdir) / "analysis/"
+        assert ana_dir.is_dir()
 
-    # check whether there are js and css files
-    assert (run_dir / "data/ui/report.bundle.js").is_file()
-    assert (run_dir / "data/ui/index.css").is_file()
-    assert (ana_dir / "2_caprieval_analysis/plotly_bundle.js").is_file()
+        # check whether there are js and css files
+        assert Path(tempdir, "data/ui/report.bundle.js").exists()
+        assert Path(tempdir, "data/ui/index.css").exists()
+        assert Path(tempdir, "analysis/2_caprieval_analysis/plotly_bundle.js").exists()

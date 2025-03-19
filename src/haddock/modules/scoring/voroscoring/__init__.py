7dd1d5f0e1af147e9d2cd9269a23347bd80e149d
Victor Reys <132575181+VGPReys@users.noreply.github.com>
2025-03-19 16:19:42 +0100

    Update how to obtain defaults.yaml filename

--- src/haddock/modules/scoring/voroscoring/__init__.py
+++ src/haddock/modules/scoring/voroscoring/__init__.py
@@ -9,6 +9,7 @@ for it to run without issue.
 from os import linesep
 from pathlib import Path
 
+from haddock.core.defaults import MODULE_DEFAULT_YAML
 from haddock.core.typing import Any, FilePath
 from haddock.modules import get_engine
 from haddock.modules.scoring import ScoringModule
@@ -18,7 +19,7 @@ from haddock.modules.scoring.voroscoring.voroscoring import (
     )
 
 RECIPE_PATH = Path(__file__).resolve().parent
-DEFAULT_CONFIG = Path(RECIPE_PATH, "defaults.yaml")
+DEFAULT_CONFIG = Path(RECIPE_PATH, MODULE_DEFAULT_YAML)
 
 
 class HaddockModule(ScoringModule):

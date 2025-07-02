6cb386ac9b8902a74f6570bf1da5567a3ea6365d
VGPReys <v.g.p.reys@uu.nl>
2025-07-02 09:27:07 +0200

    fix issue with random selection when limitting number of restraints

--- src/haddock/clis/restraints/restrain_ligand.py
+++ src/haddock/clis/restraints/restrain_ligand.py
@@ -175,10 +175,7 @@ def restrain_ligand(
         "{distance:6.3f} {deviation:.2f} {deviation:.2f}{linesep:s}"
         )
 
-    _unambig_str_list = [
-        f"! Restraints to fix {ligand_name} in its initial position{os.linesep}"
-        ]
-
+    _unambig_str_list: list[str] = []
     # Loop over all min distances
     for dist in min_dist_list:
         r_at, l_at, d = dist
@@ -201,8 +198,10 @@ def restrain_ligand(
         unambig_str_list = random.sample(_unambig_str_list, max_restraints)
     else:
         unambig_str_list = _unambig_str_list
+    
     # Concatenate into a single string
-    unambig_str = "".join(unambig_str_list)
+    unambig_str = f"! Restraints to fix {ligand_name} in its initial position{os.linesep}"
+    unambig_str += "".join(unambig_str_list)
     return unambig_str
 
 

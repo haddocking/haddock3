9e2f8b1b21fbc8865cac6cb3e7440c43c7c4299a
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-09-03 11:19:24 +0200

    update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,28 +1,43 @@
 """Define common test variables."""
+
+import pytest
 from pathlib import Path
 
 from haddock.modules import modules_category
 
 
 tests_path = Path(__file__).resolve().parents[0]
-data_folder = Path(tests_path, 'data')
-golden_data = Path(tests_path, 'golden_data')
+data_folder = Path(tests_path, "data")
+golden_data = Path(tests_path, "golden_data")
 
 # preprocessing files
-broken_pdb = Path(data_folder, 'broken.pdb')
-corrected_pdb = Path(data_folder, 'corrected.pdb')
+broken_pdb = Path(data_folder, "broken.pdb")
+corrected_pdb = Path(data_folder, "corrected.pdb")
 
-residues_top = Path(data_folder, 'residues.top')
+residues_top = Path(data_folder, "residues.top")
 
-configs_data = Path(tests_path, 'configs')
-emptycfg = Path(configs_data, 'empty.cfg')
-haddock3_yaml_cfg_examples = Path(configs_data, 'yml_example.yml')
-haddock3_yaml_converted = Path(configs_data, 'yaml2cfg_converted.cfg')
-haddock3_yaml_converted_no_header = \
-    Path(configs_data, 'yaml2cfg_converted_no_header.cfg')
-clean_steps_folder = Path(tests_path, 'clean_output_data')
+configs_data = Path(tests_path, "configs")
+emptycfg = Path(configs_data, "empty.cfg")
+haddock3_yaml_cfg_examples = Path(configs_data, "yml_example.yml")
+haddock3_yaml_converted = Path(configs_data, "yaml2cfg_converted.cfg")
+haddock3_yaml_converted_no_header = Path(
+    configs_data, "yaml2cfg_converted_no_header.cfg"
+)
+clean_steps_folder = Path(tests_path, "clean_output_data")
 
 steptmp = Path(data_folder, "0_dummystep")
 
 # defines which modules are already working
-working_modules = [t for t in modules_category.items() if t[0] != 'topocg']
+working_modules = [t for t in modules_category.items() if t[0] != "topocg"]
+
+
+try:
+    import py3Dmol
+
+    NOTEBOOK_ENABLED = True
+except ImportError:
+    NOTEBOOK_ENABLED = False
+
+has_notebook = pytest.mark.skipif(
+    not NOTEBOOK_ENABLED, reason="notebook dependencies not found"
+)

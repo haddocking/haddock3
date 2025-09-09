7cc1caaeba3b0a4642ad5e10441cc5eb4f568aef
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-09-09 17:48:15 +0200

    add test_grid [wip]

---
+++ integration_tests/test_grid.py
@@ -0,0 +1,47 @@
+from math import exp
+from haddock.libs.libgrid import GridJob
+import tempfile
+import pytest
+from pathlib import Path
+from haddock.libs.libsubprocess import CNSJob
+import time
+
+from haddock.libs.libgrid import JobStatus
+
+from . import CNS_EXEC
+
+
+@pytest.fixture
+def cnsjob():
+    """A simple CNSJob that writes a message to a file."""
+    expected_output_fname = "output.pdb"
+    cns_inp = f"""
+set display={expected_output_fname} end
+display hello_from_the_grid
+close {expected_output_fname} end
+stop"""
+
+    yield CNSJob(
+        input_file=cns_inp,
+        output_file=expected_output_fname,
+        cns_exec=CNS_EXEC,
+    )
+
+
+def test_submit_cns_job_to_grid(cnsjob):
+    instructions = """#!/bin/bash
+unzip payload.zip
+./cns input.inp 
+"""
+    j = GridJob(cnsjob, instructions)
+    j.submit()
+
+    # HACK: WAIT FOREVER
+    while j.status != JobStatus.DONE:
+        j.update_status()
+        print(j)
+        time.sleep(2)
+
+    assert j.status == JobStatus.DONE
+
+    j.retrieve_output()

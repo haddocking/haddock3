7b68d0f4574585d3dad7b0e6f68ccc31808107be
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-09-25 11:16:53 +0200

    add tests placeholder

--- tests/test_libgrid.py
+++ tests/test_libgrid.py
@@ -1,1 +1,136 @@
-# PLACEHOLDER - to be done in <https://github.com/haddocking/haddock3/issues/1385>
+import pytest
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_ping_dirac():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_validate_diract():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_jobstatus_from_string():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_package():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_create_jdl():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_update_status():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_prepare_payload():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_submit():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_retrieve_output():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_clean_timings():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_clean():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface_parse_output():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface__process_line():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridinterface__find_output():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridjob_parse_input():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridjob_create_job_script():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridjob_process_input_f():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_compositegridjob_create_job_script():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_compositegridjob_parse_input():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_compositegridjob_process_input_f():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridscheduler_run():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridscheduler_create_batches():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridscheduler_wait_for_completion():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridscheduler_submit_jobs():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridscheduler_probe_grid_efficiency():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridscheduler_calculate_optimal_batch_size():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_gridscheduler_process_jobs():
+    pass

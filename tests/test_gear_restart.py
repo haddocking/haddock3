984e406d5c65a69b1262598b39b9ae70d7783a18
VGPReys <v.g.p.reys@uu.nl>
2026-04-11 11:16:22 +0200

    tests

--- tests/test_gear_restart.py
+++ tests/test_gear_restart.py
@@ -1,9 +1,15 @@
 """Test gear.restart_run."""
 import argparse
-
+import os
 import pytest
+import tempfile
 
-from haddock.gear import restart_run
+from haddock.gear import (
+    restart_run,
+    preprocess_restart_from,
+    ANA_DIR,
+    TRACEBACK_FOLDER,
+)
 
 
 def test_has_help():
@@ -65,3 +71,38 @@ def test_restart_cli_error(n):
         ap.parse_args(f'--restart {n}'.split())
     assert exit.type == SystemExit
     assert exit.value.code == 2
+
+
+def test_preprocess_restart_from():
+    """Test removal of downstream directories."""
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Build mimic of previous run directories
+        module_dirs = ["topoaa", "rigidbody", "caprieval"]
+        for module_index, module_name in module_dirs:
+            os.makedirs(f"{tmpdir}/data/{module_index}_{module_name}")
+            os.makedirs(f"{tmpdir}/{module_index}_{module_name}")
+            if module_name == "caprieval":
+                os.makedirs(
+                    f"{tmpdir}/{ANA_DIR}/{module_index}_{module_name}_analysis"
+                    )
+        os.makedirs(f"{tmpdir}/{TRACEBACK_FOLDER}")
+        
+        # Test restart preprocessing function
+        preprocess_restart_from(tmpdir, 2)
+        
+        # Verify they were removed
+        expected_to_be_removed = [
+            f"{tmpdir}/{TRACEBACK_FOLDER}",
+            f"{tmpdir}/{ANA_DIR}/2_caprieval_analysis",
+            f"{tmpdir}/2_caprieval",
+        ]
+        expected_to_stay_there = [
+            f"{tmpdir}/1_rigidbody",
+            f"{tmpdir}/data/1_rigidbody",
+            f"{tmpdir}/0_topoaa",
+            f"{tmpdir}/data/0_topoaa",
+        ]
+        for should_not_exist in expected_to_be_removed:
+            assert not os.path.exists(should_not_exist)
+        for should_exist in expected_to_stay_there:
+            assert os.path.exists(should_exist)

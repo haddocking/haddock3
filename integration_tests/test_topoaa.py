8e4163e6c0a30127b837dacc0facbac4d1acf901
amjjbonvin <alexandre.bonvin@gmail.com>
2026-06-29 12:47:17 +0200

    Added support for 1-O-methyl-2-N-Acetyl-alpha-D-glucopyranose - issue #1593

--- integration_tests/test_topoaa.py
+++ integration_tests/test_topoaa.py
@@ -487,6 +487,32 @@ def test_topoaa_GalNAcOMe(topoaa_module):
     assert " C9 " in file_content
 
 
+def test_topoaa_GalNAcalphaOMe(topoaa_module):
+    """Test the topoaa module with GalNAc-alpha-OMe."""
+    topoaa_module.params["molecules"] = [
+        Path(GOLDEN_DATA, "gal-naa.pdb"),
+    ]
+    topoaa_module.params["cns_exec"] = CNS_EXEC
+    topoaa_module.params["debug"] = True
+
+    topoaa_module.run()
+
+    expected_inp = Path(topoaa_module.path, "gal-naa.inp")
+    expected_psf = Path(topoaa_module.path, "gal-naa_haddock.psf")
+    expected_pdb = Path(topoaa_module.path, "gal-naa_haddock.pdb")
+    expected_gz = Path(topoaa_module.path, "gal-naa.out.gz")
+
+    assert expected_inp.exists(), f"{expected_inp} does not exist"
+    assert expected_psf.exists(), f"{expected_psf} does not exist"
+    assert expected_gz.exists(), f"{expected_gz} does not exist"
+    assert expected_pdb.exists(), f"{expected_pdb} does not exist"
+
+    with open(expected_pdb, encoding="utf-8", mode="r") as f:
+        file_content = f.read()
+
+    assert " C9 " in file_content
+
+
 def test_topoaa_module_protein_noCter(topoaa_module):
     """Topoaa module with uncharged Cter and charged Nter."""
     topoaa_module.params["molecules"] = [

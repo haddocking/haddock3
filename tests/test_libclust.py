102cb0508794d446b63625501ff0bc628c6aaf28
Rodrigo V. Honorato <rvhonorato@protonmail.com>
2026-08-05 12:03:16 +0200

    add `test_add_cluster_info`

--- tests/test_libclust.py
+++ tests/test_libclust.py
@@ -1,15 +1,19 @@
 """Test the libclust library."""
+
 import os
-from pathlib import Path
-import pytest
 import random
 import tempfile
+from pathlib import Path
+from typing import cast
+
+import pytest
 
 from haddock.libs.libclust import (
     MAX_NB_ENTRY_HTML_MATRIX,
+    add_cluster_info,
     plot_cluster_matrix,
     write_structure_list,
-    )
+)
 from haddock.libs.libontology import PDBFile
 
 from . import golden_data
@@ -20,16 +24,12 @@ def protprot_input_models():
     """Prot-prot input."""
     return [
         PDBFile(
-            Path(golden_data, "protprot_complex_1.pdb"),
-            path=golden_data,
-            score=-42
-            ),
+            Path(golden_data, "protprot_complex_1.pdb"), path=golden_data, score=-42
+        ),
         PDBFile(
-            Path(golden_data, "protprot_complex_2.pdb"),
-            path=golden_data,
-            score=-17
-            )
-        ]
+            Path(golden_data, "protprot_complex_2.pdb"), path=golden_data, score=-17
+        ),
+    ]
 
 
 @pytest.fixture
@@ -38,7 +38,7 @@ def big_distance_matrix_data():
     bigmatrix = []
     for i in range(1, MAX_NB_ENTRY_HTML_MATRIX + 1):
         for j in range(i + 1, MAX_NB_ENTRY_HTML_MATRIX + 2):
-            bigmatrix.append(f'{i}\t{j}\t{random.random():.2f}')
+            bigmatrix.append(f"{i}\t{j}\t{random.random():.2f}")
     return bigmatrix
 
 
@@ -49,7 +49,7 @@ def small_distance_matrix_data():
     matrix = []
     for i in range(1, small):
         for j in range(i + 1, small + 1):
-            matrix.append(f'{i}\t{j}\t{random.random():.2f}')
+            matrix.append(f"{i}\t{j}\t{random.random():.2f}")
     return matrix
 
 
@@ -62,11 +62,11 @@ def test_write_structure_list(protprot_input_models):
     write_structure_list(protprot_input_models, clustered_models, cl_fname)
     observed_file_content = open(cl_fname, "r").read()
     expected_file_content = (
-        f'rank\tmodel_name\tscore\tcluster_id{os.linesep}'
-        f'1\tprotprot_complex_1.pdb\t-42.00\t1{os.linesep}'
-        f'2\tprotprot_complex_2.pdb\t-17.00\t-{os.linesep}'
-        f'{os.linesep}'
-        )
+        f"rank\tmodel_name\tscore\tcluster_id{os.linesep}"
+        f"1\tprotprot_complex_1.pdb\t-42.00\t1{os.linesep}"
+        f"2\tprotprot_complex_2.pdb\t-17.00\t-{os.linesep}"
+        f"{os.linesep}"
+    )
     assert observed_file_content == expected_file_content
     os.unlink(cl_fname)
 
@@ -75,9 +75,9 @@ def test_plot_cluster_matrix_big(big_distance_matrix_data):
     """Test test_plot_cluster_matrix_big function with big matrix."""
     with tempfile.TemporaryDirectory(dir=".") as tmpdir:
         # Write matrix
-        matrix_path = f'{tmpdir}bigmatrix.mat'
-        with open(matrix_path, 'w') as f:
-            f.write('\n'.join(big_distance_matrix_data))
+        matrix_path = f"{tmpdir}bigmatrix.mat"
+        with open(matrix_path, "w") as f:
+            f.write("\n".join(big_distance_matrix_data))
         # Run function
         figure_path = plot_cluster_matrix(
             matrix_path,
@@ -88,7 +88,7 @@ def test_plot_cluster_matrix_big(big_distance_matrix_data):
             color_scale="Blues",
             reverse=False,
             output_fname="clust_matrix_test_big",
-            )
+        )
         # Check output
         assert figure_path is None
         assert not Path("clust_matrix_test_big.html").exists()
@@ -99,23 +99,60 @@ def test_plot_cluster_matrix(small_distance_matrix_data):
     """Test test_plot_cluster_matrix_big function with small matrix."""
     with tempfile.TemporaryDirectory(dir=".") as tmpdir:
         # Write matrix
-        matrix_path = f'{tmpdir}smallmatrix.mat'
-        with open(matrix_path, 'w') as f:
-            f.write('\n'.join(small_distance_matrix_data))
+        matrix_path = f"{tmpdir}smallmatrix.mat"
+        with open(matrix_path, "w") as f:
+            f.write("\n".join(small_distance_matrix_data))
         # Run function
         figure_path = plot_cluster_matrix(
             matrix_path,
             list(range(10)),
             [str(i + 1) for i in range(10)],
-            dttype='random',
+            dttype="random",
             diag_fill=0.5,
             color_scale="Blues",
             reverse=True,
-            output_fname='clust_matrix_test_small',
-            )
+            output_fname="clust_matrix_test_small",
+        )
         # Check output
         assert os.path.exists(figure_path)
         assert Path(figure_path).stat().st_size != 0
-        assert Path(figure_path).suffix == '.html'
+        assert Path(figure_path).suffix == ".html"
         Path(figure_path).unlink(missing_ok=False)
         Path(matrix_path).unlink(missing_ok=False)
+
+
+def test_add_cluster_info(protprot_input_models):
+    # create cluster information
+    # [(cluster_id, average-score)]
+    input_l = [(1, +1.0), (2, -1.0)]
+
+    # Create some PDBs
+    input_pdbs = [
+        PDBFile(file_name=f"{i}.pdb", score=float(random.randint(-500, 0)))
+        for i in range(10)
+    ]
+
+    # Make sure the PDBFiles are not created with any cluster information
+    for p in input_pdbs:
+        assert p.clt_id is None
+        assert p.clt_rank is None
+        assert p.clt_model_rank is None
+
+    # split so we get two sets of models
+    mid = len(input_pdbs) // 2
+    pdbs_cluster_1 = input_pdbs[mid:]
+    pdbs_cluster_2 = input_pdbs[:mid]
+
+    observed = add_cluster_info(input_l, clt_dic={1: pdbs_cluster_1, 2: pdbs_cluster_2})
+
+    # assert that the cluster information was added to the models
+    for p in observed:
+        assert p.clt_id is not None
+        assert p.clt_rank is not None
+        assert p.clt_model_rank is not None
+
+    # assert the order has changed
+    assert observed[0] != input_pdbs[0]
+
+    # check they were properly sorted
+    assert cast(float, observed[0].score) < cast(float, observed[-1].score)
